6a44463916c021aae3afddfb8fcea0bd6823ac27
HashEngineering <hashengineeringsolutions@gmail.com>
2016-02-27 01:29:17 -0800

    Updated project files.

---
+++ core/src/main/java/org/bitcoinj/core/MasternodePayments.java
@@ -0,0 +1,7 @@
+package org.bitcoinj.core;
+
+/**
+ * Created by Eric on 2/21/2016.
+ */
+public class MasternodePayments {
+}
---
+++ core/src/main/java/org/bitcoinj/kits/LevelDBWalletAppKit.java
@@ -0,0 +1,7 @@
+package org.bitcoinj.kits;
+
+/**
+ * Created by Eric on 2/23/2016.
+ */
+public class LevelDBWalletAppKit {
+}
