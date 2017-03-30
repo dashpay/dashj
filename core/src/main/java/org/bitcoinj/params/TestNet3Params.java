48a268f3abb35403814f5ec44d112da1cdc46dfd
HashEngineering <hashengineeringsolutions@gmail.com>
2017-03-29 23:08:10 -0700

    Update xpub prefix for testnet3

--- core/src/main/java/org/bitcoinj/params/TestNet3Params.java
+++ core/src/main/java/org/bitcoinj/params/TestNet3Params.java
@@ -68,8 +68,8 @@ public class TestNet3Params extends AbstractBitcoinNetParams {
         dnsSeeds = CoinDefinition.testnetDnsSeeds;
 
         addrSeeds = null;
-        bip32HeaderPub = 0x3a59ea14;
-        bip32HeaderPriv = 0x3a58f342;
+        bip32HeaderPub = 0x043587cf;
+        bip32HeaderPriv = 0x04358394 ;
 
         strSporkKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
 
