bd2b07631722e33b1f7478bc58c754e159972004
HashEngineering <hashengineeringsolutions@gmail.com>
2017-06-04 10:31:18 -0700

    Update difficulty bits of default Block constructor used for genesis block and unit tests

--- core/src/main/java/org/bitcoinj/core/Block.java
+++ core/src/main/java/org/bitcoinj/core/Block.java
@@ -120,7 +120,7 @@ public class Block extends Message {
         super(params);
         // Set up a few basic things. We are not complete after this though.
         version = setVersion;
-        difficultyTarget = 0x1d07fff8L;
+        difficultyTarget = 0x1e0fffffL;
         time = System.currentTimeMillis() / 1000;
         prevBlockHash = Sha256Hash.ZERO_HASH;
 
