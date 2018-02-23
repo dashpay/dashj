77931439efab500314c0d7d1b40cba12d9eec7ff
Andreas Schildbach <andreas.schildbach@bloq.com>
2018-02-23 11:09:41 -0800

    Remove some unused imports.
    (cherry picked from commit 71a5b1b89fbec2ea541ceb91d50562c22f5b5196)

--- core/src/test/java/org/bitcoinj/wallet/WalletTest.java
+++ core/src/test/java/org/bitcoinj/wallet/WalletTest.java
@@ -62,7 +62,6 @@ import com.google.protobuf.ByteString;
 import org.bitcoinj.wallet.Protos.Wallet.EncryptionType;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
