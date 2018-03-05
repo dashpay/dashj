112160db13e011ac37e9184a5d3bb00a72f4c996
Andreas Schildbach <andreas.schildbach@bloq.com>
2018-03-05 07:07:57 -0800

    Consistently use standard acronyms: P2PKH, P2PK, P2SH
    (cherry picked from commit b9a31104af4efbbae62b7da63d3299cd8352cc6c)
    Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

--- core/src/main/java/org/bitcoinj/core/Transaction.java
+++ core/src/main/java/org/bitcoinj/core/Transaction.java
@@ -165,7 +165,7 @@ public class Transaction extends ChildMessage {
     public static final Coin DEFAULT_TX_FEE = Coin.valueOf(CoinDefinition.DEFAULT_MIN_TX_FEE); // 0.5 mBTC
 
     /**
-     * Any standard (ie pay-to-address) output smaller than this value (in satoshis) will most likely be rejected by the network.
+     * Any standard (ie P2PKH) output smaller than this value (in satoshis) will most likely be rejected by the network.
      * This is calculated by assuming a standard output will be 34 bytes, and then using the formula used in
      * {@link TransactionOutput#getMinNonDustValue(Coin)}.
      */
