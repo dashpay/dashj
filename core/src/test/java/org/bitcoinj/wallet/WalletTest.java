dc2e9ddf49415920b738a2e04b76b15ddd07a596
Andreas Schildbach <andreas.schildbach@bloq.com>
2018-03-05 07:36:02 -0800

    Remove concept of stateful transaction signers.
    (cherry picked from commit 9d6090a7eaa3f81bbba9caa6ebc1966da9ca728c)
    Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

--- core/src/test/java/org/bitcoinj/wallet/WalletTest.java
+++ core/src/test/java/org/bitcoinj/wallet/WalletTest.java
@@ -41,7 +41,6 @@ import org.bitcoinj.script.Script;
 import org.bitcoinj.script.ScriptBuilder;
 import org.bitcoinj.script.ScriptChunk;
 import org.bitcoinj.script.ScriptPattern;
-import org.bitcoinj.signers.StatelessTransactionSigner;
 import org.bitcoinj.signers.TransactionSigner;
 import org.bitcoinj.store.BlockStoreException;
 import org.bitcoinj.store.MemoryBlockStore;
@@ -3321,16 +3320,6 @@ public class WalletTest extends TestWithWallet {
     }
 
     @Test
-    public void transactionSignersShouldBeSerializedAlongWithWallet() throws Exception {
-        TransactionSigner signer = new NopTransactionSigner(true);
-        wallet.addTransactionSigner(signer);
-        assertEquals(2, wallet.getTransactionSigners().size());
-        wallet = roundTrip(wallet);
-        assertEquals(2, wallet.getTransactionSigners().size());
-        assertTrue(wallet.getTransactionSigners().get(1).isReady());
-    }
-
-    @Test
     public void watchingMarriedWallet() throws Exception {
         DeterministicKey watchKey = wallet.getWatchingKey();
         String serialized = watchKey.serializePubB58(UNITTEST);
@@ -3341,7 +3330,7 @@ public class WalletTest extends TestWithWallet {
         final DeterministicKeyChain keyChain = new DeterministicKeyChain(new SecureRandom());
         DeterministicKey partnerKey = DeterministicKey.deserializeB58(null, keyChain.getWatchingKey().serializePubB58(UNITTEST), UNITTEST);
 
-        TransactionSigner signer = new StatelessTransactionSigner() {
+        TransactionSigner signer = new TransactionSigner() {
             @Override
             public boolean isReady() {
                 return true;
