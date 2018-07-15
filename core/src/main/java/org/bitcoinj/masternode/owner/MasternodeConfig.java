4ec6d3dca0f648f34efe3c92a19fad38d03f9155
HashEngineering <hashengineeringsolutions@gmail.com>
2018-07-14 17:18:21 -0700

    MasternodeControl:  Add masternode management and voting

---
+++ core/src/main/java/org/bitcoinj/masternode/owner/MasternodeConfig.java
@@ -0,0 +1,207 @@
+package org.bitcoinj.masternode.owner;
+
+import com.google.common.base.Preconditions;
+import org.bitcoinj.core.Context;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.Utils;
+import org.bitcoinj.params.MainNetParams;
+import org.bitcoinj.utils.Pair;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * Created by Hash Engineering on 7/8/2018.
+ */
+public class MasternodeConfig {
+
+
+    public static class MasternodeEntry {
+
+        private String alias;
+        private String ip;
+        private String privKey;
+        private String txHash;
+        private String outputIndex;
+
+        public MasternodeEntry(String alias, String ip, String privKey, String txHash, String outputIndex) {
+            this.alias = alias;
+            this.ip = ip;
+            this.privKey = privKey;
+            this.txHash = txHash;
+            this.outputIndex = outputIndex;
+        }
+
+        public String getAlias() {
+            return alias;
+        }
+
+        public void setAlias(String alias) {
+            this.alias = alias;
+        }
+
+        public String getOutputIndex() {
+            return outputIndex;
+        }
+
+        public void setOutputIndex(String outputIndex) {
+            this.outputIndex = outputIndex;
+        }
+
+        public String getPrivKey() {
+            return privKey;
+        }
+
+        public void setPrivKey(String privKey) {
+            this.privKey = privKey;
+        }
+
+        public String getTxHash() {
+            return txHash;
+        }
+
+        public void setTxHash(String txHash) {
+            this.txHash = txHash;
+        }
+
+        public String getIp() {
+            return ip;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+    }
+
+    protected File masternodeConfigFile;
+
+    public MasternodeConfig(String configFile) {
+        entries = new ArrayList<MasternodeEntry>();
+        masternodeConfigFile = new File(configFile);
+    }
+
+    public void add(String alias, String ip, String privKey, String txHash, String outputIndex) {
+        Preconditions.checkNotNull(alias);
+        Preconditions.checkNotNull(ip);
+        Preconditions.checkNotNull(privKey);
+        Preconditions.checkNotNull(txHash);
+        Preconditions.checkNotNull(outputIndex);
+        MasternodeEntry cme = new MasternodeEntry(alias, ip, privKey, txHash, outputIndex);
+        entries.add(cme);
+    }
+
+    public ArrayList<MasternodeEntry> getEntries() {
+        return entries;
+    }
+
+    public int getCount() {
+        return entries.size();
+    }
+
+    private ArrayList<MasternodeEntry> entries;
+
+    public boolean importFromFile(String fileToImport, StringBuilder strError) {
+        try {
+            //TODO: copy file to correct location, then read()
+            File importFile = new File(fileToImport);
+            FileInputStream inStream = new FileInputStream(importFile);
+            FileOutputStream outStream = new FileOutputStream(masternodeConfigFile);
+            byte [] buffer = new byte [(int)importFile.length()];
+            inStream.read(buffer);
+            outStream.write(buffer);
+            inStream.close();
+            outStream.close();
+            return true;
+        } catch (FileNotFoundException x) {
+            strError.append(x.getMessage());
+            return false;
+        } catch (IOException x) {
+            strError.append(x.getMessage());
+            return false;
+        }
+    }
+
+    public boolean read(StringBuilder strError) {
+        int linenumber = 1;
+
+        try {
+            FileReader fileReader = new FileReader(masternodeConfigFile);
+            BufferedReader stream = new BufferedReader(fileReader);
+
+
+            String line;
+            while ((line = stream.readLine()) != null) {
+
+                String[] parts = line.split(" ");
+
+                String comment;
+                String alias;
+                String ip;
+                String privKey;
+                String txHash;
+                String outputIndex;
+
+                if (parts.length == 0 || parts[0].trim().startsWith("#"))
+                    continue;
+
+                if (parts.length >= 5) {
+                    alias = parts[0];
+                    ip = parts[1];
+                    privKey = parts[2];
+                    txHash = parts[3];
+                    outputIndex = parts[4];
+                } else {
+                    strError.append("Could not parse masternode.conf" + "\n" + String.format("Line: %d", linenumber) + "\n\"" + line + "\"");
+                    stream.close();
+                    return false;
+                }
+
+
+                Pair<Integer, String> result = Utils.splitHostPort(ip);
+                int port = result.getFirst();
+                String hostname = result.getSecond();
+                if (port == 0 || hostname.equals("")) {
+                    strError.append("Failed to parse host:port string" + "\n" + String.format("Line: %d", linenumber) + "\n\"" + line + "\"");
+                    stream.close();
+                    return false;
+                }
+                int mainnetDefaultPort = MainNetParams.get().getPort();
+                if (Context.get().getParams().getId().equals(NetworkParameters.ID_MAINNET)) {
+                    if (port != mainnetDefaultPort) {
+                        strError.append("Invalid port detected in masternode.conf" + "\n" + String.format("Port: %d", port) + "\n" + String.format("Line: %d", linenumber) + "\n\"" + line + "\"" + "\n" + String.format("(must be %d for mainnet)", mainnetDefaultPort));
+                        stream.close();
+                        return false;
+                    }
+                } else if (port == mainnetDefaultPort) {
+                    strError.append("Invalid port detected in masternode.conf" + "\n" + String.format("Line: %d", linenumber) + "\n\"" + line + "\"" + "\n" + String.format("(%d could be used only on mainnet)", mainnetDefaultPort));
+                    stream.close();
+                    return false;
+                }
+
+
+                add(alias, ip, privKey, txHash, outputIndex);
+            }
+            stream.close();
+            return true;
+        } catch (FileNotFoundException x) {
+            strError.append(x.getMessage());
+
+            try {
+                FileOutputStream configFile = new FileOutputStream(masternodeConfigFile, true);
+                String strHeader = "# Masternode config file\n" + "# Format: alias IP:port masternodeprivkey collateral_output_txid collateral_output_index\n" + "# Example: mn1 127.0.0.2:19999 93HaYBVUCYjEMeeH1Y4sBGLALQZE1Yc1K64xiqgX37tGBDQL8Xg 2bcd3c84c84f87eaa86e4e56834c92927a07f9e18718810b92e0d0324456a67c 0\n";
+                configFile.write(strHeader.getBytes());
+                return true; // Nothing to read, so just return
+            } catch (FileNotFoundException x2) {
+                strError.append(x2.getMessage());
+                return false;
+            } catch (IOException x2) {
+                strError.append(x2.getMessage());
+                return false;
+            }
+        } catch (IOException x) {
+            strError.append(x.getMessage());
+            return false;
+        }
+    }
+}
+
---
+++ core/src/main/java/org/bitcoinj/masternode/owner/MasternodeControl.java
@@ -0,0 +1,249 @@
+package org.bitcoinj.masternode.owner;
+
+import org.bitcoinj.core.*;
+import org.bitcoinj.governance.GovernanceException;
+import org.bitcoinj.governance.GovernanceVote;
+import org.bitcoinj.governance.GovernanceVoteBroadcast;
+import org.bitcoinj.governance.GovernanceVoting;
+
+import java.util.ArrayList;
+
+import static org.bitcoinj.governance.GovernanceVote.VoteOutcome.VOTE_OUTCOME_NONE;
+import static org.bitcoinj.governance.GovernanceVote.VoteSignal.VOTE_SIGNAL_NONE;
+
+/**
+ * Created by Eric on 7/8/2018.
+ */
+public class MasternodeControl {
+    /**
+     * The Config.
+     */
+    MasternodeConfig masternodeConfig;
+    /**
+     * The Context.
+     */
+    Context context;
+
+    /**
+     * Instantiates a new Masternode control.
+     *
+     * @param context              the context
+     * @param masternodeConfigFile the masternode config file name
+     */
+    public MasternodeControl(Context context, String masternodeConfigFile) {
+        this.context = context;
+        masternodeConfig = new MasternodeConfig(masternodeConfigFile);
+    }
+
+    public boolean load() {
+        StringBuilder errorMessage = new StringBuilder();
+        return masternodeConfig.read(errorMessage);
+    }
+
+    /**
+     * Vote with a single masternode identified by alias.  Returns true if successful.
+     *
+     * @param alias          the alias
+     * @param governanceHash the hash of the governance object
+     * @param voteSignal     the vote signal [funding|valid|delete]
+     * @param voteOutcome    the vote outcome [yes|no|abstain]
+     * @param errorMessage   the error message with details on failure
+     * @return true if successful
+     */
+    public GovernanceVoteBroadcast voteAlias(String alias, String governanceHash, String voteSignal, String voteOutcome, StringBuilder errorMessage) {
+        Sha256Hash hash = Sha256Hash.wrap(Utils.HEX.decode(governanceHash));
+
+        // CONVERT NAMED SIGNAL/ACTION AND CONVERT
+
+        GovernanceVote.VoteSignal eVoteSignal = GovernanceVoting.convertVoteSignal(voteSignal);
+        if(eVoteSignal == VOTE_SIGNAL_NONE) {
+            errorMessage.append("Invalid vote signal ("+voteSignal+"). Please using one of the following: " +
+                    "(funding|valid|delete|endorsed)");
+            return null;
+        }
+
+        GovernanceVote.VoteOutcome eVoteOutcome = GovernanceVoting.convertVoteOutcome(voteOutcome);
+        if(eVoteOutcome == VOTE_OUTCOME_NONE) {
+            errorMessage.append("Invalid vote outcome("+voteOutcome+"). Please use one of the following: 'yes', 'no' or 'abstain'");
+            return null;
+        }
+
+        // EXECUTE VOTE FOR EACH MASTERNODE, COUNT SUCCESSES VS FAILURES
+
+        int nSuccessful = 0;
+        int nFailed = 0;
+        GovernanceVoteBroadcast broadcast = null;
+
+
+        for (MasternodeConfig.MasternodeEntry mne : masternodeConfig.getEntries()) {
+            // IF WE HAVE A SPECIFIC NODE REQUESTED TO VOTE, DO THAT
+            if (!alias.equals(mne.getAlias())) continue;
+
+            // INIT OUR NEEDED VARIABLES TO EXECUTE THE VOTE
+            StringBuilder strError = new StringBuilder();
+            MasternodeSignature vchMasterNodeSignature;
+            String strMasterNodeSignMessage;
+
+            PublicKey pubKeyCollateralAddress;
+            ECKey keyCollateralAddress;
+            PublicKey pubKeyMasternode;
+            ECKey keyMasternode;
+
+            // SETUP THE SIGNING KEY FROM MASTERNODE.CONF ENTRY
+
+            keyMasternode = MessageSigner.getKeysFromSecret(mne.getPrivKey(), strError);
+            if (keyMasternode == null) {
+                nFailed++;
+                errorMessage.append(alias + String.format("-- failure --Invalid masternode key %s.\n", mne.getPrivKey()));
+                continue;
+            }
+            pubKeyMasternode = new PublicKey(keyMasternode.getPubKey());
+
+            // SEARCH FOR THIS MASTERNODE ON THE NETWORK, THE NODE MUST BE ACTIVE TO VOTE
+
+            Sha256Hash nTxHash = Sha256Hash.wrap(Utils.HEX.decode(mne.getTxHash()));
+
+            int nOutputIndex = 0;
+            try {
+                nOutputIndex = Integer.parseInt(mne.getOutputIndex());
+            } catch (NumberFormatException x) {
+                continue;
+            }
+
+            TransactionOutPoint outpoint = new TransactionOutPoint(context.getParams(), nOutputIndex, nTxHash);
+
+            Masternode mn = context.masternodeManager.get(outpoint);
+
+            if(mn == null) {
+                nFailed++;
+                errorMessage.append(alias + "-- failure --Masternode must be publicly available on network to vote. Masternode not found.\n");
+                continue;
+            }
+
+            // CREATE NEW GOVERNANCE OBJECT VOTE WITH OUTCOME/SIGNAL
+
+            GovernanceVote vote = new GovernanceVote(context.getParams(), outpoint, hash, eVoteSignal, eVoteOutcome);
+            if(!vote.sign(keyMasternode, pubKeyMasternode)) {
+                nFailed++;
+                errorMessage.append(alias + "-- failure --Failure to sign\n");
+                continue;
+            }
+
+            // UPDATE LOCAL DATABASE WITH NEW OBJECT SETTINGS
+
+            GovernanceException exception = new GovernanceException();
+            if(context.governanceManager.processVoteAndRelay(vote, exception)) {
+                nSuccessful++;
+                errorMessage.append(alias + "voting successful\n");
+                broadcast = context.peerGroup.broadcastGovernanceVote(vote);
+            } else {
+                nFailed++;
+                errorMessage.append(alias + "-- failure --" + exception.getMessage() + "\n");
+            }
+        }
+
+        // REPORT STATS TO THE USER
+
+        errorMessage.append("overall result : " + String.format("Voted successfully %d time(s) and failed %d time(s).", nSuccessful, nFailed));
+
+        return broadcast;
+    }
+
+    /**
+     * Vote many boolean.
+     *
+     * @param alias          the alias
+     * @param governanceHash the governance hash
+     * @param voteSignal     the vote signal
+     * @param voteOutcome    the vote outcome
+     * @param errorMessage   the error message with details on failure
+     * @return true if all voting is successful
+     */
+    public boolean voteMany(String alias, String governanceHash, String voteSignal, String voteOutcome, StringBuilder errorMessage) {
+        Sha256Hash hash = Sha256Hash.wrap(Utils.HEX.decode(governanceHash));
+
+        // CONVERT NAMED SIGNAL/ACTION AND CONVERT
+
+        GovernanceVote.VoteSignal eVoteSignal = GovernanceVoting.convertVoteSignal(voteSignal);
+        if(eVoteSignal == VOTE_SIGNAL_NONE) {
+            errorMessage.append("Invalid vote signal ("+voteSignal+"). Please using one of the following: " +
+                    "(funding|valid|delete|endorsed)");
+            return false;
+        }
+
+        GovernanceVote.VoteOutcome eVoteOutcome = GovernanceVoting.convertVoteOutcome(voteOutcome);
+        if(eVoteOutcome == VOTE_OUTCOME_NONE) {
+            errorMessage.append("Invalid vote outcome("+voteOutcome+"). Please use one of the following: 'yes', 'no' or 'abstain'");
+            return false;
+        }
+
+        int nSuccessful = 0;
+        int nFailed = 0;
+
+
+        for (MasternodeConfig.MasternodeEntry mne : masternodeConfig.getEntries()) {
+            StringBuilder strError = new StringBuilder();
+            MasternodeSignature vchMasterNodeSignature;
+            String strMasterNodeSignMessage;
+
+            PublicKey pubKeyCollateralAddress;
+            ECKey keyCollateralAddress;
+            PublicKey pubKeyMasternode;
+            ECKey keyMasternode;
+
+            keyMasternode = MessageSigner.getKeysFromSecret(mne.getPrivKey(), strError);
+            if (keyMasternode == null) {
+                nFailed++;
+                errorMessage.append(alias + String.format("-- failure --Invalid masternode key %s.\n", mne.getPrivKey()));
+                continue;
+            }
+            pubKeyMasternode = new PublicKey(keyMasternode.getPubKey());
+
+            Sha256Hash nTxHash = Sha256Hash.wrap(Utils.HEX.decode(mne.getTxHash()));
+
+            int nOutputIndex = 0;
+            try {
+                nOutputIndex = Integer.parseInt(mne.getOutputIndex());
+            } catch (NumberFormatException x) {
+                continue;
+            }
+
+            TransactionOutPoint outpoint = new TransactionOutPoint(context.getParams(), nOutputIndex, nTxHash);
+
+            Masternode mn = context.masternodeManager.get(outpoint);
+
+            if(mn == null) {
+                nFailed++;
+                errorMessage.append(alias + "-- failure --Masternode must be publicly available on network to vote. Masternode not found.\n");
+                continue;
+            }
+
+            // CREATE NEW GOVERNANCE OBJECT VOTE WITH OUTCOME/SIGNAL
+
+            GovernanceVote vote = new GovernanceVote(context.getParams(), outpoint, hash, eVoteSignal, eVoteOutcome);
+            if(!vote.sign(keyMasternode, pubKeyMasternode)) {
+                nFailed++;
+                errorMessage.append(alias + "-- failure --Failure to sign\n");
+                continue;
+            }
+
+            // UPDATE LOCAL DATABASE WITH NEW OBJECT SETTINGS
+
+            GovernanceException exception = new GovernanceException();
+            if(context.governanceManager.processVoteAndRelay(vote, exception)) {
+                nSuccessful++;
+                errorMessage.append(alias + "voting successful\n");
+            } else {
+                nFailed++;
+                errorMessage.append(alias + "-- failure --" + exception.getMessage() + "\n");
+            }
+        }
+
+        // REPORT STATS TO THE USER
+
+        errorMessage.append("overall result : " + String.format("Voted successfully %d time(s) and failed %d time(s).", nSuccessful, nFailed));
+
+        return nFailed == 0;
+    }
+
+}
