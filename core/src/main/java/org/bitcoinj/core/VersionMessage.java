fc600a00011c77c85b309936572d31fd5107e876
HashEngineering <hashengineeringsolutions@gmail.com>
2018-07-29 00:07:35 -0700

    Start 0.15-SNAPSHOT

--- core/src/main/java/org/bitcoinj/core/VersionMessage.java
+++ core/src/main/java/org/bitcoinj/core/VersionMessage.java
@@ -81,6 +81,7 @@ public class VersionMessage extends Message {
 
     /** The version of this library release, as a string. */
     public static final String BITCOINJ_VERSION = "0.15-SNAPSHOT";
+
     /** The value that is prepended to the subVer field of this application. */
     public static final String LIBRARY_SUBVER = "/"+CoinDefinition.coinName+"J:" + BITCOINJ_VERSION + "/";
 
