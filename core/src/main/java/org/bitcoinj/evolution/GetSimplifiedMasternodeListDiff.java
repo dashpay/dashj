91547b9e81cd9a8f7d58a8e372607aaa2541ed2f
Hash Engineering Solutions <hashengineeringsolutions@gmail.com>
2018-09-10 23:06:09 -0700

    Add GetSimplifiedMasternodeListDiff message to query MN list differences

---
+++ core/src/main/java/org/bitcoinj/evolution/GetSimplifiedMasternodeListDiff.java
@@ -0,0 +1,40 @@
+package org.bitcoinj.evolution;
+
+import org.bitcoinj.core.Message;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.ProtocolException;
+import org.bitcoinj.core.Sha256Hash;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class GetSimplifiedMasternodeListDiff extends Message {
+
+    Sha256Hash baseBlockHash;
+    Sha256Hash blockHash;
+
+    public static int MESSAGE_SIZE = 64;
+
+    public GetSimplifiedMasternodeListDiff(Sha256Hash baseBlockHash, Sha256Hash blockHash) {
+        this.baseBlockHash = baseBlockHash;
+        this.blockHash = blockHash;
+        length = MESSAGE_SIZE;
+    }
+
+    public GetSimplifiedMasternodeListDiff(NetworkParameters params, byte [] payload) {
+        super(params, payload, 0);
+    }
+
+    @Override
+    protected void parse() throws ProtocolException {
+        baseBlockHash = readHash();
+        blockHash = readHash();
+        length = cursor - offset;
+    }
+
+    @Override
+    protected void bitcoinSerializeToStream(OutputStream stream) throws IOException {
+        stream.write(baseBlockHash.getReversedBytes());
+        stream.write(blockHash.getReversedBytes());
+    }
+}
