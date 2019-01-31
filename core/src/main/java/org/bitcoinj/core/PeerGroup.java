a4fb506358fdb9622ec20f018104e19d2cf622c6
Andreas Schildbach <andreas.schildbach@bloq.com>
2019-01-31 09:17:30 -0800

    PeerGroup: Fix NullPointerException in handleNewPeer() if no download peer could be selected.
    (cherry picked from commit e3e4e3f32a186a71babb42b0faff504d366b0c70)
    Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

--- core/src/main/java/org/bitcoinj/core/PeerGroup.java
+++ core/src/main/java/org/bitcoinj/core/PeerGroup.java
@@ -1534,11 +1534,14 @@ public class PeerGroup implements TransactionBroadcaster, GovernanceVoteBroadcas
             for (Wallet wallet : wallets)
                 peer.addWallet(wallet);
             if (downloadPeer == null) {
-                // Kick off chain download if we aren't already doing it.
-                setDownloadPeer(selectDownloadPeer(peers));
-                boolean shouldDownloadChain = downloadListener != null && chain != null;
-                if (shouldDownloadChain) {
-                    startBlockChainDownloadFromPeer(downloadPeer);
+                Peer newDownloadPeer = selectDownloadPeer(peers);
+                if (newDownloadPeer != null) {
+                    setDownloadPeer(newDownloadPeer);
+                    // Kick off chain download if we aren't already doing it.
+                    boolean shouldDownloadChain = downloadListener != null && chain != null;
+                    if (shouldDownloadChain) {
+                        startBlockChainDownloadFromPeer(downloadPeer);
+                    }
                 }
             }
             // Make sure the peer knows how to upload transactions that are requested from us.
