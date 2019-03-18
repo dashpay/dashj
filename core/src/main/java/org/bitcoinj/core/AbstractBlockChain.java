aa7d2eb9feffc0343aaca4fafd444da1df72b12e
Premek <44365000+pvyhnal-generalbytes@users.noreply.github.com>
2019-03-18 04:05:10 -0700

    AbstractBlockChain: Try connecting orphans more often in add().
    This fixes a memory leak.
    
    (cherry picked from commit 10607fb9f5bdd8a3ab31a766843e89ee3bd78da5)

--- core/src/main/java/org/bitcoinj/core/AbstractBlockChain.java
+++ core/src/main/java/org/bitcoinj/core/AbstractBlockChain.java
@@ -466,17 +466,16 @@ public abstract class AbstractBlockChain {
                 checkState(tryConnecting, "bug in tryConnectingOrphans");
                 log.warn("Block does not connect: {} prev {}", block.getHashAsString(), block.getPrevBlockHash());
                 orphanBlocks.put(block.getHash(), new OrphanBlock(block, filteredTxHashList, filteredTxn));
-                tryConnectingOrphans();
+                if (tryConnecting)
+                    tryConnectingOrphans();
                 return false;
             } else {
                 checkState(lock.isHeldByCurrentThread());
                 // It connects to somewhere on the chain. Not necessarily the top of the best known chain.
                 params.checkDifficultyTransitions(storedPrev, block, blockStore);
                 connectBlock(block, storedPrev, shouldVerifyTransactions(), filteredTxHashList, filteredTxn);
-
                 if (tryConnecting)
                     tryConnectingOrphans();
-
                 return true;
             }
         } finally {
