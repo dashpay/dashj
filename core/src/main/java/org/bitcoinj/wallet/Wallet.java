719d839d4085874fd3f0ff6d0372a1df96b8850f
Matthew Leon <ml@matthewleon.com>
2019-03-24 21:31:13 -0400

    Avoid use of deprecated Throwables.propagate().
    (cherry picked from commit de8a1f755b8dbaf73aa482b7636a47538dfe5ec4)

--- core/src/main/java/org/bitcoinj/wallet/Wallet.java
+++ core/src/main/java/org/bitcoinj/wallet/Wallet.java
@@ -18,7 +18,6 @@
 package org.bitcoinj.wallet;
 
 import com.google.common.annotations.*;
-import com.google.common.base.*;
 import com.google.common.collect.*;
 import com.google.common.primitives.*;
 import com.google.common.util.concurrent.*;
@@ -5301,7 +5300,7 @@ public class Wallet extends BaseTaggableObject
                 keyChainExtensions.remove(extension.getWalletExtensionID());
             else
             extensions.remove(extension.getWalletExtensionID());
-            Throwables.propagate(throwable);
+            throw throwable;
         } finally {
             keyChainGroupLock.unlock();
             lock.unlock();
