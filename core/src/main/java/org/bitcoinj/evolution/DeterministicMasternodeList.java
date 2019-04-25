c47e8a782f40582942f1904c0e5db5ec557f5add
HashEngineering <hashengineeringsolutions@gmail.com>
2019-04-24 23:03:54 -0700

    Add DeterministicMasternode classes for Full Verification

---
+++ core/src/main/java/org/bitcoinj/evolution/DeterministicMasternodeList.java
@@ -0,0 +1,171 @@
+package org.bitcoinj.evolution;
+
+import com.google.common.base.Preconditions;
+import org.bitcoinj.core.*;
+import org.bitcoinj.utils.Pair;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DeterministicMasternodeList extends Message {
+
+    private Sha256Hash blockHash;
+    private long height;
+    HashMap<Sha256Hash, DeterministicMasternode> mnMap;
+    HashMap<Sha256Hash, Pair<Sha256Hash, Integer>> mnUniquePropertyMap;
+
+    DeterministicMasternodeList(NetworkParameters params, byte [] payload) {
+        super(params, payload, 0);
+    }
+
+    DeterministicMasternodeList(DeterministicMasternodeList other) {
+        this.blockHash = other.blockHash;
+        this.height = other.height;
+
+        mnMap = new HashMap<Sha256Hash, DeterministicMasternode>(other.mnMap.size());
+        for(Map.Entry<Sha256Hash, DeterministicMasternode> entry : mnMap.entrySet()) {
+            mnMap.put(entry.getKey(), new DeterministicMasternode(entry.getValue()));
+        }
+    }
+
+    @Override
+    protected void parse() throws ProtocolException {
+        blockHash = readHash();
+        height = (int)readUint32();
+        int size = (int)readVarInt();
+        mnMap = new HashMap<Sha256Hash, DeterministicMasternode>(size);
+        for(int i = 0; i < size; ++i)
+        {
+            Sha256Hash hash = readHash();
+            DeterministicMasternode mn = new DeterministicMasternode(params, payload, cursor);
+            cursor += mn.getMessageSize();
+            mnMap.put(hash, mn);
+        }
+
+        size = (int)readVarInt();
+        mnUniquePropertyMap = new HashMap<Sha256Hash, Pair<Sha256Hash, Integer>>(size);
+        for(long i = 0; i < size; ++i)
+        {
+            Sha256Hash hash = readHash();
+            Sha256Hash first = readHash();
+            int second = (int)readUint32();
+            mnUniquePropertyMap.put(hash, new Pair<Sha256Hash, Integer>(first, second));
+        }
+    }
+
+    @Override
+    protected void bitcoinSerializeToStream(OutputStream stream) throws IOException {
+        stream.write(blockHash.getReversedBytes());
+        Utils.uint32ToByteStreamLE(height, stream);
+
+        stream.write(new VarInt(mnMap.size()).encode());
+        for(Map.Entry<Sha256Hash, DeterministicMasternode> entry : mnMap.entrySet()) {
+            stream.write(entry.getKey().getReversedBytes());
+            entry.getValue().bitcoinSerializeToStream(stream);
+        }
+        stream.write(new VarInt(mnMap.size()).encode());
+        for(Map.Entry<Sha256Hash, Pair<Sha256Hash, Integer>> entry : mnUniquePropertyMap.entrySet()) {
+            stream.write(entry.getKey().getReversedBytes());
+            stream.write(entry.getValue().getFirst().getReversedBytes());
+            Utils.uint32ToByteStreamLE(entry.getValue().getSecond().intValue(), stream);
+        }
+    }
+
+    DeterministicMasternodeList applyDiff(DeterministicMasternodeListDiff diff)
+    {
+        Preconditions.checkState(diff.prevBlockHash.equals(blockHash) && diff.height == height + 1);
+
+        DeterministicMasternodeList result = new DeterministicMasternodeList(this);
+        result.blockHash = diff.blockHash;
+        result.height = diff.height;
+
+        for (Sha256Hash hash : diff.removedMNs) {
+            result.removeMN(hash);
+        }
+        for (Map.Entry<Sha256Hash, DeterministicMasternode> entry : diff.addedMNs.entrySet()) {
+            result.addMN(entry.getValue());
+        }
+        for (Map.Entry<Sha256Hash, DeterministicMasternodeState> entry : diff.updatedMNs.entrySet()) {
+            result.updateMN(entry.getKey(), entry.getValue());
+        }
+
+        return result;
+    }
+
+    void addMN(DeterministicMasternode dmn)
+    {
+        Preconditions.checkState(!mnMap.containsKey(dmn.proTxHash));
+        mnMap.put(dmn.proTxHash, dmn);
+        addUniqueProperty(dmn, dmn.state.address);
+        addUniqueProperty(dmn, dmn.state.keyIDOwner);
+        addUniqueProperty(dmn, dmn.state.pubKeyOperator);
+    }
+    void updateMN(Sha256Hash proTxHash, DeterministicMasternodeState state)
+    {
+        DeterministicMasternode oldDmn = mnMap.get(proTxHash);
+        Preconditions.checkState(oldDmn != null);
+        DeterministicMasternode dmn = oldDmn;
+        DeterministicMasternodeState oldState = dmn.state;
+        dmn.state = state;
+        mnMap.put(proTxHash, dmn);
+
+        updateUniqueProperty(dmn, oldState.address, state.address);
+        updateUniqueProperty(dmn, oldState.keyIDOwner, state.keyIDOwner);
+        updateUniqueProperty(dmn, oldState.pubKeyOperator, state.pubKeyOperator);
+    }
+    void removeMN(Sha256Hash proTxHash)
+    {
+        DeterministicMasternode dmn = getMN(proTxHash);
+        assert(dmn != null);
+        deleteUniqueProperty(dmn, dmn.state.address);
+        deleteUniqueProperty(dmn, dmn.state.keyIDOwner);
+        deleteUniqueProperty(dmn, dmn.state.pubKeyOperator);
+        mnMap.remove(proTxHash);
+    }
+
+    public DeterministicMasternode getMN(Sha256Hash proTxHash)
+    {
+        DeterministicMasternode p = mnMap.get(proTxHash);
+        if (p == null) {
+            return null;
+        }
+        return p;
+    }
+
+
+    <T extends ChildMessage> void addUniqueProperty(DeterministicMasternode dmn, T value)
+    {
+        Sha256Hash hash = value.getHash();
+        int i = 1;
+        Pair<Sha256Hash, Integer> oldEntry = mnUniquePropertyMap.get(hash);
+        assert(oldEntry == null || oldEntry.getFirst().equals(dmn.proTxHash));
+        if(oldEntry != null)
+            i = oldEntry.getSecond() + 1;
+        Pair<Sha256Hash, Integer> newEntry = new Pair(dmn.proTxHash, i);
+
+        mnUniquePropertyMap.put(hash, newEntry);
+    }
+    <T extends ChildMessage>
+    void deleteUniqueProperty(DeterministicMasternode dmn, T oldValue)
+    {
+        Sha256Hash oldHash = oldValue.getHash();
+        Pair<Sha256Hash, Integer> p = mnUniquePropertyMap.get(oldHash);
+        assert(p != null && p.getFirst() == dmn.proTxHash);
+        if (p.getSecond() == 1) {
+            mnUniquePropertyMap.remove(oldHash);
+        } else {
+            mnUniquePropertyMap.put(oldHash, new Pair<Sha256Hash, Integer>(dmn.proTxHash, p.getSecond() - 1));
+        }
+    }
+    <T extends ChildMessage>
+    void updateUniqueProperty(DeterministicMasternode dmn, T oldValue, T newValue)
+    {
+        if (oldValue == newValue) {
+            return;
+        }
+        deleteUniqueProperty(dmn, oldValue);
+        addUniqueProperty(dmn, newValue);
+    }
+}
---
+++ core/src/main/java/org/bitcoinj/evolution/DeterministicMasternodeState.java
@@ -0,0 +1,121 @@
+package org.bitcoinj.evolution;
+
+import org.bitcoinj.core.*;
+import org.bitcoinj.crypto.BLSPublicKey;
+import org.bitcoinj.script.Script;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class DeterministicMasternodeState extends ChildMessage {
+    int registeredHeight;//{-1};
+    int lastPaidHeight;//{0};
+    int PoSePenality;//{0};
+    int PoSeRevivedHeight;//{-1};
+    int PoSeBanHeight;//{-1};
+    short revocationReason;//{ProUpRevTx::REASON_NOT_SPECIFIED};
+
+    Sha256Hash confirmedHash;
+    Sha256Hash confirmedHashWithProRegTxHash;
+
+    KeyId keyIDOwner;
+    KeyId keyIDOperator;
+    BLSPublicKey pubKeyOperator;
+    KeyId keyIDVoting;
+    MasternodeAddress address;
+    Script scriptPayout;
+    Script scriptOperatorPayout;
+
+    DeterministicMasternodeState(NetworkParameters params, byte [] payload, int offset) {
+        super(params, payload, offset);
+    }
+
+    public DeterministicMasternodeState(DeterministicMasternodeState other) {
+        registeredHeight = other.registeredHeight;
+        lastPaidHeight = other.lastPaidHeight;
+        PoSePenality = other.PoSePenality;
+        PoSeRevivedHeight = other.PoSeRevivedHeight;
+        PoSeBanHeight = other.PoSeBanHeight;
+        revocationReason = other.revocationReason;
+        keyIDOwner = other.keyIDOwner.duplicate();
+        pubKeyOperator = other.pubKeyOperator;
+        keyIDVoting = other.keyIDVoting.duplicate();
+        address = other.address.duplicate();
+        scriptPayout = new Script(other.scriptPayout.getProgram());
+        scriptOperatorPayout = new Script(other.scriptOperatorPayout.getProgram());
+    }
+
+    @Override
+    protected void parse() throws ProtocolException {
+        registeredHeight = (int)readUint32();
+        lastPaidHeight = (int)readUint32();
+        PoSePenality = (int)readUint32();
+        PoSeRevivedHeight = (int)readUint32();
+        PoSeBanHeight = (int)readUint32();
+        revocationReason = (short)readUint16();
+        confirmedHash = readHash();
+        confirmedHashWithProRegTxHash = readHash();
+        keyIDOwner = new KeyId(params, payload, cursor);
+        cursor += keyIDOwner.getMessageSize();
+        if(!params.isSupportingEvolution()) {
+            keyIDOperator = new KeyId(params, payload, cursor);
+            cursor += keyIDOperator.getMessageSize();
+        } else {
+            pubKeyOperator = new BLSPublicKey(params, payload, cursor);
+            cursor += pubKeyOperator.getMessageSize();
+        }
+        keyIDVoting = new KeyId(params, payload, cursor);
+        cursor += keyIDVoting.getMessageSize();
+        address = new MasternodeAddress(params, payload, cursor, 0);
+        cursor += address.getMessageSize();
+        scriptPayout = new Script(readByteArray());
+        scriptOperatorPayout = new Script(readByteArray());
+    }
+
+    @Override
+    protected void bitcoinSerializeToStream(OutputStream stream) throws IOException {
+        Utils.uint32ToByteStreamLE(registeredHeight, stream);
+        Utils.uint32ToByteStreamLE(lastPaidHeight, stream);
+        Utils.uint32ToByteStreamLE(PoSePenality, stream);
+        Utils.uint32ToByteStreamLE(PoSeRevivedHeight, stream);
+        Utils.uint32ToByteStreamLE(PoSeBanHeight, stream);
+        Utils.uint16ToByteStreamLE(revocationReason, stream);
+        stream.write(confirmedHash.getReversedBytes());
+        stream.write(confirmedHashWithProRegTxHash.getReversedBytes());
+        keyIDOwner.bitcoinSerialize(stream);
+        if(!params.isSupportingEvolution()) {
+            keyIDOperator.bitcoinSerialize(stream);
+        } else {
+            pubKeyOperator.bitcoinSerialize(stream);
+        }
+        keyIDVoting.bitcoinSerialize(stream);
+        address.bitcoinSerialize(stream);
+        Utils.bytesToByteStream(scriptPayout.getProgram(), stream);
+        Utils.bytesToByteStream(scriptOperatorPayout.getProgram(), stream);
+    }
+
+    public String toString()
+    {
+        String payoutAddress = "unknown";
+        String operatorRewardAddress = "none";
+        try {
+            payoutAddress = scriptPayout.getToAddress(params).toString();
+        } catch (AddressFormatException x) {
+
+        } catch (ScriptException x) {
+
+        }
+        try {
+            operatorRewardAddress = scriptOperatorPayout.getToAddress(params).toString();
+        } catch (AddressFormatException x) {
+
+        } catch (ScriptException x) {
+
+        }
+
+        return String.format("DeterministicMNState(registeredHeight=%d, lastPaidHeight=%d, PoSePenality=%d, PoSeRevivedHeight=%d, PoSeBanHeight=%d, revocationReason=%d, "+
+                "keyIDOwner=%s, pubkeyOperator=%s, keyIDVoting=%s, addr=%s, payoutAddress=%s, operatorRewardAddress=%s)",
+                registeredHeight, lastPaidHeight, PoSePenality, PoSeRevivedHeight, PoSeBanHeight, revocationReason,
+                keyIDOwner, pubKeyOperator, keyIDVoting, address.toString(), payoutAddress, operatorRewardAddress);
+    }
+}
