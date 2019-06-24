0ab363d68883c4e44dafeeb442e03a0d6c2a08a7
HashEngineering <hashengineeringsolutions@gmail.com>
2019-06-23 22:33:48 -0700

    SporkManager:  Remove Spork 15 activation actions
    * DMN Lists are already on by default
    * These commands were resulting in a DMN List reset
    
    Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

--- core/src/main/java/org/bitcoinj/core/SporkManager.java
+++ core/src/main/java/org/bitcoinj/core/SporkManager.java
@@ -178,12 +178,7 @@ public class SporkManager {
     {
         //correct fork via spork technology
         if(nSporkID == SPORK_12_RECONSIDER_BLOCKS && nValue > 0) {
-            reprocessBlocks((int)nValue);
-        } else if(nSporkID == SPORK_15_DETERMINISTIC_MNS_ENABLED) {
-            if(nValue <= context.blockChain.getBestChainHeight())
-                context.masternodeListManager.updateMNList();
-            if(context.getParams().isSupportingEvolution())
-                context.peerGroup.setMinRequiredProtocolVersionAndDisconnect(NetworkParameters.ProtocolVersion.DMN_LIST.getBitcoinProtocolVersion());
+            reprocessBlocks((int) nValue);
         } else if(nSporkID == SPORK_17_QUORUM_DKG_ENABLED) {
             context.masternodeListManager.resetMNList();
         }
