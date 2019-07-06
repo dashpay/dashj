638bc21ebc87ac0dcb342d99328eb6b2576696cd
HashEngineering <hashengineeringsolutions@gmail.com>
2019-07-06 16:13:40 -0700

    BLS:  Add unit tests from Chia-Network

--- bls/src/test/java/org/dashj/bls/BaseTest.java
+++ bls/src/test/java/org/dashj/bls/BaseTest.java
@@ -1,16 +1,15 @@
 package org.dashj.bls;
 
-import static org.junit.Assert.fail;
-
 /**
  * Created by hashengineering on 11/13/18.
  */
 public class BaseTest {
     static {
+        BLS.Init();/*
         try {
             System.loadLibrary(JNI.LIBRARY_NAME);
         } catch (UnsatisfiedLinkError x) {
             fail(x.getMessage());
-        }
+        }*/
     }
 }
