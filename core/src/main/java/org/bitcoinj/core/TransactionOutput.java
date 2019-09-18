bitcoinj/bitcoinj

ScriptPattern: Rename the matchers and extraction helpers for "pay to" script types into an abbreviated form.

core/src/main/java/org/bitcoinj/core/TransactionOutput.java
@@ -122,17 +122,17 @@ public class TransactionOutput extends ChildMessage {
     @Nullable
     @Deprecated
     public LegacyAddress getAddressFromP2PKHScript(NetworkParameters params) throws ScriptException {
-        if (ScriptPattern.isPayToPubKeyHash(getScriptPubKey()))
+        if (ScriptPattern.isP2PKH(getScriptPubKey()))
             return LegacyAddress.fromPubKeyHash(params,
-                    ScriptPattern.extractHashFromPayToPubKeyHash(getScriptPubKey()));
+                    ScriptPattern.extractHashFromP2PKH(getScriptPubKey()));
         return null;
     }
 
     @Nullable
     @Deprecated
     public LegacyAddress getAddressFromP2SH(NetworkParameters params) throws ScriptException {
-        if (ScriptPattern.isPayToScriptHash(getScriptPubKey()))
-            return LegacyAddress.fromScriptHash(params, ScriptPattern.extractHashFromPayToScriptHash(getScriptPubKey()));
+        if (ScriptPattern.isP2SH(getScriptPubKey()))
+            return LegacyAddress.fromScriptHash(params, ScriptPattern.extractHashFromP2SH(getScriptPubKey()));
         return null;
     }
 
@@ -301,15 +301,15 @@ public class TransactionOutput extends ChildMessage {
     public boolean isMine(TransactionBag transactionBag) {
         try {
             Script script = getScriptPubKey();
-            if (ScriptPattern.isPayToPubKey(script))
-                return transactionBag.isPubKeyMine(ScriptPattern.extractKeyFromPayToPubKey(script));
-            else if (ScriptPattern.isPayToScriptHash(script))
-                return transactionBag.isPayToScriptHashMine(ScriptPattern.extractHashFromPayToScriptHash(script));
-            else if (ScriptPattern.isPayToPubKeyHash(script))
-                return transactionBag.isPubKeyHashMine(ScriptPattern.extractHashFromPayToPubKeyHash(script),
+            if (ScriptPattern.isP2PK(script))
+                return transactionBag.isPubKeyMine(ScriptPattern.extractKeyFromP2PK(script));
+            else if (ScriptPattern.isP2SH(script))
+                return transactionBag.isPayToScriptHashMine(ScriptPattern.extractHashFromP2SH(script));
+            else if (ScriptPattern.isP2PKH(script))
+                return transactionBag.isPubKeyHashMine(ScriptPattern.extractHashFromP2PKH(script),
                         Script.ScriptType.P2PKH);
-            else if (ScriptPattern.isPayToWitnessPubKeyHash(script))
-                return transactionBag.isPubKeyHashMine(ScriptPattern.extractHashFromPayToWitnessHash(script),
+            else if (ScriptPattern.isP2WPKH(script))
+                return transactionBag.isPubKeyHashMine(ScriptPattern.extractHashFromP2WH(script),
                         Script.ScriptType.P2WPKH);
             else
                 return false;
@@ -329,11 +329,11 @@ public class TransactionOutput extends ChildMessage {
             Script script = getScriptPubKey();
             StringBuilder buf = new StringBuilder("TxOut of ");
             buf.append(Coin.valueOf(value).toFriendlyString());
-            if (ScriptPattern.isPayToPubKeyHash(script) || ScriptPattern.isPayToWitnessPubKeyHash(script)
-                    || ScriptPattern.isPayToScriptHash(script))
+            if (ScriptPattern.isP2PKH(script) || ScriptPattern.isP2WPKH(script)
+                    || ScriptPattern.isP2SH(script))
                 buf.append(" to ").append(script.getToAddress(params));
-            else if (ScriptPattern.isPayToPubKey(script))
-                buf.append(" to pubkey ").append(Utils.HEX.encode(ScriptPattern.extractKeyFromPayToPubKey(script)));
+            else if (ScriptPattern.isP2PK(script))
+                buf.append(" to pubkey ").append(Utils.HEX.encode(ScriptPattern.extractKeyFromP2PK(script)));
             else if (ScriptPattern.isSentToMultisig(script))
                 buf.append(" to multisig");
             else

core/src/test/java/org/bitcoinj/script/ScriptTest.java
@@ -80,7 +80,7 @@ public class ScriptTest {
         byte[] pubkeyBytes = HEX.decode(pubkeyProg);
         Script pubkey = new Script(pubkeyBytes);
         assertEquals("DUP HASH160 PUSHDATA(20)[33e81a941e64cda12c6a299ed322ddbdd03f8d0e] EQUALVERIFY CHECKSIG", pubkey.toString());
-        Address toAddr = LegacyAddress.fromPubKeyHash(TESTNET, ScriptPattern.extractHashFromPayToPubKeyHash(pubkey));
+        Address toAddr = LegacyAddress.fromPubKeyHash(TESTNET, ScriptPattern.extractHashFromP2PKH(pubkey));
         assertEquals("mkFQohBpy2HDXrCwyMrYL5RtfrmeiuuPY2", toAddr.toString());
     }
 
@@ -113,14 +113,14 @@ public class ScriptTest {
     @Test
     public void testP2SHOutputScript() throws Exception {
         Address p2shAddress = LegacyAddress.fromBase58(MAINNET, "35b9vsyH1KoFT5a5KtrKusaCcPLkiSo1tU");
-        assertTrue(ScriptPattern.isPayToScriptHash(ScriptBuilder.createOutputScript(p2shAddress)));
+        assertTrue(ScriptPattern.isP2SH(ScriptBuilder.createOutputScript(p2shAddress)));
     }
 
     @Test
     public void testIp() throws Exception {
         byte[] bytes = HEX.decode("41043e96222332ea7848323c08116dddafbfa917b8e37f0bdf63841628267148588a09a43540942d58d49717ad3fabfe14978cf4f0a8b84d2435dad16e9aa4d7f935ac");
         Script s = new Script(bytes);
-        assertTrue(ScriptPattern.isPayToPubKey(s));
+        assertTrue(ScriptPattern.isP2PK(s));
     }
     
     @Test
@@ -446,7 +446,7 @@ public class ScriptTest {
         // pay to script hash
         Script p2shScript = ScriptBuilder.createP2SHOutputScript(new byte[20]);
         Address scriptAddress = LegacyAddress.fromScriptHash(TESTNET,
-                ScriptPattern.extractHashFromPayToScriptHash(p2shScript));
+                ScriptPattern.extractHashFromP2SH(p2shScript));
         assertEquals(scriptAddress, p2shScript.getToAddress(TESTNET, true));
     }
 

dashpay/dashj

(cherry picked from commit fffa4c06fe73c1fd1f1ef8688415ccd9f73ac060)
Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

core/src/main/java/org/bitcoinj/core/TransactionOutput.java
@@ -122,17 +122,17 @@ public class TransactionOutput extends ChildMessage {
     @Nullable
     @Deprecated
     public Address getAddressFromP2PKHScript(NetworkParameters params) throws ScriptException {
-        if (ScriptPattern.isPayToPubKeyHash(getScriptPubKey()))
+        if (ScriptPattern.isP2PKH(getScriptPubKey()))
             return Address.fromPubKeyHash(params,
-                    ScriptPattern.extractHashFromPayToPubKeyHash(getScriptPubKey()));
+                    ScriptPattern.extractHashFromP2PKH(getScriptPubKey()));
         return null;
     }
 
     @Nullable
     @Deprecated
     public Address getAddressFromP2SH(NetworkParameters params) throws ScriptException {
-        if (ScriptPattern.isPayToScriptHash(getScriptPubKey()))
-            return Address.fromScriptHash(params, ScriptPattern.extractHashFromPayToScriptHash(getScriptPubKey()));
+        if (ScriptPattern.isP2SH(getScriptPubKey()))
+            return Address.fromScriptHash(params, ScriptPattern.extractHashFromP2SH(getScriptPubKey()));
         return null;
     }
 
@@ -301,12 +301,12 @@ public class TransactionOutput extends ChildMessage {
     public boolean isMine(TransactionBag transactionBag) {
         try {
             Script script = getScriptPubKey();
-            if (ScriptPattern.isPayToPubKey(script))
-                return transactionBag.isPubKeyMine(ScriptPattern.extractKeyFromPayToPubKey(script));
-            else if (ScriptPattern.isPayToScriptHash(script))
-                return transactionBag.isPayToScriptHashMine(ScriptPattern.extractHashFromPayToScriptHash(script));
-            else if (ScriptPattern.isPayToPubKeyHash(script))
-                return transactionBag.isPubKeyHashMine(ScriptPattern.extractHashFromPayToPubKeyHash(script),
+            if (ScriptPattern.isP2PK(script))
+                return transactionBag.isPubKeyMine(ScriptPattern.extractKeyFromP2PK(script));
+            else if (ScriptPattern.isP2SH(script))
+                return transactionBag.isPayToScriptHashMine(ScriptPattern.extractHashFromP2SH(script));
+            else if (ScriptPattern.isP2PKH(script))
+                return transactionBag.isPubKeyHashMine(ScriptPattern.extractHashFromP2PKH(script),
                         Script.ScriptType.P2PKH);
             else
                 return false;
@@ -326,11 +326,11 @@ public class TransactionOutput extends ChildMessage {
             Script script = getScriptPubKey();
             StringBuilder buf = new StringBuilder("TxOut of ");
             buf.append(Coin.valueOf(value).toFriendlyString());
-            if (ScriptPattern.isPayToPubKeyHash(script)
-                    || ScriptPattern.isPayToScriptHash(script))
+            if (ScriptPattern.isP2PKH(script)
+                    || ScriptPattern.isP2SH(script))
                 buf.append(" to ").append(script.getToAddress(params));
-            else if (ScriptPattern.isPayToPubKey(script))
-                buf.append(" to pubkey ").append(Utils.HEX.encode(ScriptPattern.extractKeyFromPayToPubKey(script)));
+            else if (ScriptPattern.isP2PK(script))
+                buf.append(" to pubkey ").append(Utils.HEX.encode(ScriptPattern.extractKeyFromP2PK(script)));
             else if (ScriptPattern.isSentToMultisig(script))
                 buf.append(" to multisig");
             else

core/src/test/java/org/bitcoinj/script/ScriptTest.java
@@ -80,7 +80,7 @@ public class ScriptTest {
         byte[] pubkeyBytes = HEX.decode(pubkeyProg);
         Script pubkey = new Script(pubkeyBytes);
         assertEquals("DUP HASH160 PUSHDATA(20)[33e81a941e64cda12c6a299ed322ddbdd03f8d0e] EQUALVERIFY CHECKSIG", pubkey.toString());
-        Address toAddr = Address.fromPubKeyHash(TESTNET, ScriptPattern.extractHashFromPayToPubKeyHash(pubkey));
+        Address toAddr = Address.fromPubKeyHash(TESTNET, ScriptPattern.extractHashFromP2PKH(pubkey));
         assertEquals("yR3uMqqBZFidFRFTgXWnPiKhvVF1EtUZ16", toAddr.toString());
     }
 
@@ -113,14 +113,14 @@ public class ScriptTest {
     @Test
     public void testP2SHOutputScript() throws Exception {
         Address p2shAddress = Address.fromBase58(MAINNET, "7WJnm5FSpJttSr72bWWqFFZrXwB8ZzsK7b");
-        assertTrue(ScriptPattern.isPayToScriptHash(ScriptBuilder.createOutputScript(p2shAddress)));
+        assertTrue(ScriptPattern.isP2SH(ScriptBuilder.createOutputScript(p2shAddress)));
     }
 
     @Test
     public void testIp() throws Exception {
         byte[] bytes = HEX.decode("41043e96222332ea7848323c08116dddafbfa917b8e37f0bdf63841628267148588a09a43540942d58d49717ad3fabfe14978cf4f0a8b84d2435dad16e9aa4d7f935ac");
         Script s = new Script(bytes);
-        assertTrue(ScriptPattern.isPayToPubKey(s));
+        assertTrue(ScriptPattern.isP2PK(s));
     }
     
     @Test
@@ -446,7 +446,7 @@ public class ScriptTest {
         // pay to script hash
         Script p2shScript = ScriptBuilder.createP2SHOutputScript(new byte[20]);
         Address scriptAddress = Address.fromScriptHash(TESTNET,
-                ScriptPattern.extractHashFromPayToScriptHash(p2shScript));
+                ScriptPattern.extractHashFromP2PKH(p2shScript));
         assertEquals(scriptAddress, p2shScript.getToAddress(TESTNET, true));
     }
 