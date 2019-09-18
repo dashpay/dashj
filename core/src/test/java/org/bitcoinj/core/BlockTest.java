bitcoinj/bitcoinj

Remove TestNet2.

core/src/test/java/org/bitcoinj/core/BlockTest.java
@@ -21,7 +21,6 @@ import com.google.common.io.ByteStreams;
 
 import org.bitcoinj.core.AbstractBlockChain.NewBlockType;
 import org.bitcoinj.params.MainNetParams;
-import org.bitcoinj.params.TestNet2Params;
 import org.bitcoinj.params.TestNet3Params;
 import org.bitcoinj.params.UnitTestParams;
 import org.bitcoinj.script.Script;
@@ -32,61 +31,54 @@ import org.bitcoinj.wallet.Wallet.BalanceType;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
 
-import static org.bitcoinj.core.Utils.HEX;
 import static org.junit.Assert.*;
 
 public class BlockTest {
-    private static final NetworkParameters TESTNET2 = TestNet2Params.get();
-    private static final NetworkParameters TESTNET3 = TestNet3Params.get();
+    private static final NetworkParameters TESTNET = TestNet3Params.get();
     private static final NetworkParameters UNITTEST = UnitTestParams.get();
     private static final NetworkParameters MAINNET = MainNetParams.get();
 
-    public static final byte[] blockBytes;
-
-    static {
-        // Block 00000000a6e5eb79dcec11897af55e90cd571a4335383a3ccfbc12ec81085935
-        // One with lots of transactions in, so a good test of the merkle tree hashing.
-        blockBytes = HEX.decode("0100000040f11b68435988807d64dff20261f7d9827825fbb37542601fb94d45000000000f28f7c69e2669981f92ff081c129e196200c60f4fad7911d93a682de0b49ea2ecd9d24c1844011d00d361050c01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff07041844011d0142ffffffff0100f2052a01000000434104a313febd5f91b6a13bd9c5317030518fee96d1319a0eb10076917294933d09c17dc1588a06953a264738f2acea0c66b99e796caa4f28158e0dd5f6fed69a185bac000000000100000001aa18a952c3f73e5d7440bc570b2aa78f72059887b25b6a1790514b7feedec090000000008b483045022100a970ee6e96fa8bea1cf76d3bda3fb70441a6ec50014d4ea3adcdeae9fbfb5129022025ce9e090366dd6175071a0a5b4a4727571b9bd7bdd5a74d3d3bad7f63eb5dd4014104ac44bdf511477465cb70fef1d06b9241e74d26047ccbdfa641ec9a0115ad35594cbb58a61a6fd56893a405bcffbf6555995ddedc7e6cd4e5ceb83a37e1cf8f98ffffffff02004d92d86a0000001976a914b8083945473bc8289efb681f94de7b07a5b851ad88ac00743ba40b0000001976a914ef01911c9efec6799d1ee5f7c6fb072d9669da8088ac000000000100000001438bd97cb2172e0dd6f341e455e00b7d089747bd4e7f54bd802afe6a6d006c7c000000008a47304402207db94026c96572519101a08e2c864bbe51c987eda6266079a35286df68f123ca02202d7d24c616776a70cce6cb2f97a424e47c30d466e96b750ca03564810249073c014104880286646dab4c894a5ff1bf62bd80047a50b86446b326f2155de94a54d01f9058d4cbc7452563a7c18b2bfb353262fc5adac6307a9446e8c4669daa58e97071ffffffff0200743ba40b0000001976a914fce443c743b456606d1e70ff0d98c4609addc10688ac00ba1dd2050000001976a91411e3e67c08e5d791c97b3d49a8d52025d3f78d3a88ac000000000100000001dc4a6300b6eca8d7ab8e119e9fc4b18890c0e26ec950e681b8d5e46c214aee24010000008b48304502202bcf8632a11192f6b4998343c13589771e6715a080236087dcb1771cbab01809022100edcc38488dd70cd38c058994f143ca5d259071b8fe54c66bf67e55d4468dcacb01410475106e33e14e9cf35bc359dd4120b580ecf5412bb8803f2a927aecd4218d1346e242c7056dca2e4c114fcf2f60799bc5e79107bd1a8b8d5135c92f02bdb59834ffffffff0200f2052a010000001976a9146c9715e09fb00ba84af1ff916ff409b4a5dc9ae288ac00c817a8040000001976a914f7be161206700eb7be1bca5768232c61e4694f4788ac000000000100000001b6cc12ff76247895cb7a604d888012136f06bba64654262044ecb93ff7762c2f000000008b48304502206d795045622c7cdfb4a211c5b41d477920437c21e69214ab4a14f10fe0306b78022100840e55114d6922f3c5e44c7cdcf85dc800d1caef64e7846998423e4ba86714e6014104f88ae9067bc05136cb53a8c18f8549f544ff55ab87ada8f3ba7e2aea773ec73585b61f18ade1c0ddd6c447788578be5fb785c245a64d29b7ff5d28b85cbec58cffffffff0200743ba40b0000001976a914c8081083a8b741da2da260bc0656b88c7bfa6fbf88ac00743ba40b0000001976a914fce443c743b456606d1e70ff0d98c4609addc10688ac0000000001000000019a8d70c7a27560b28dfe778db9ce7f2ff235faf98d5123c07991682be90a4c16000000008b483045022100a118c34f63854ee03d15cca2918d592c295035c42e03be7b0c7e86e66d40ea790220558336d2583a1da00ed5bcad2de5d3b9d485431f702bf2f002267b35ab0b41a0014104f88ae9067bc05136cb53a8c18f8549f544ff55ab87ada8f3ba7e2aea773ec73585b61f18ade1c0ddd6c447788578be5fb785c245a64d29b7ff5d28b85cbec58cffffffff0200743ba40b0000001976a914a440ef00c2e1d39be93607da66568caa26e0501888ac00743ba40b0000001976a914e1d3e65f78f962c4e9dfd04db2119aeefa4e111088ac000000000100000001883acd4bff920f19c4e570e6b3e2d7503d1072d3ca098a124e23534ecdc879d5000000008a473044022040677305de69fd8c18e2c54d5b3c67c5c05735cf6b73d420ccd306762c4bfda2022032cd32ac15ac1820265ffce82654a6008cda22a79fb619ebb65e0af806e14f9b0141044423ef78a2859eb57c4a59dc0878141cf5a4b1fdef71d649d3fb5cf8ea6b1114f4086e5d684a0999d4435db99217a994cc3cf7ad435c8f4e44613d9d160916c4ffffffff0100743ba40b0000001976a914fce443c743b456606d1e70ff0d98c4609addc10688ac000000000100000001ceb27fb142ce3bf9a1f263653dc3971332c71dd10e0e83d647037f608c459f12000000008b4830450220389218287e87d0d7b7113eb20cc1cbf1a00d7acdca32bba7f184cd066db74d6a022100b0998058e5a242699a48f931004cf5550f4e8802b866ce1baf1a0b2616861f27014104255a048d416984101c17514a89289a7d5d3dc8c562850c7a3599f0c7c39bcf9c3a43df75e1e614e51d70c5f85212c99298a21f087be93ecba7ef3900d02c0e8bffffffff0200743ba40b0000001976a914211fd13b614521ed566ddd42738381e42c3c2b2088ac00d956345f0000001976a914d3cc345ba8bdf51d7097955f0f259731f4c34f4388ac000000000100000001703701493f08e82bf6d8cb7c517070eee9f62d14904e14636a7b4af4f34180c7010000008a4730440220061a61eae90ffcf13c10c88a88c085b02954f488823c2f5c81e83a5a833e9f3b02204a61498a9668b2793e77fe3b68585f2daff4dd5daf6097a82615035325ada4730141040db6308d6170333e2c50dee4c9f18f0ab84a7a5c4c88a6836a91f39cb8f4712e08bd72979c542d4b3b60e8dc2021c1b3cc45ffaa83f36a9dec3c4473ea2aa2f3ffffffff0200f2052a010000001976a9143e7e087b9b09149e0266b7a416da2709b4ccf58788ac00d6117e030000001976a914777af71a3b2a48e48f2e467f65028d85c1b5eb5288ac0000000001000000014bdc82abc7db9c06613a712e488685c6feb4522d25017b856222171c17d144e0000000008b4830450221009eb7edcbf8d6be63529264b07bb9f40cf1a0ca779235999e40f5311d70706f1102207f65c5f66982519e6d82e13ca3e61f4f071c73da6c5830b3c4461252012b474e0141045af9665878e6696fd069669951acc54a87c5e3b256a9e20cd8858e0dc5a8c53624e0c979096c00af8a8c60136eef9ffa3d511309417b8315b7f9e3e41e805e8fffffffff0100743ba40b0000001976a914e1d3e65f78f962c4e9dfd04db2119aeefa4e111088ac000000000100000001a854b2b84a76e43de59db647121cdfe481bd8ae9623a345c2188369775b533f7010000008c493046022100c4db6ecf679264c9b525628ec5a983710ff45a1d2d4aa0b54ee218ca9a1ad4df022100dc2e0077cfdd3cbeb28f7463632902ad5306f6d5c77c8149e5b9249bfea8060e014104f9a476b612bb9788c64b9b1e4c9d2deaae1ef0baf6eb593a95d00e2ef8a2beb897ea1fb7c3832e842dd6307fd162816c19c8f458fd8dae331dbc9062fb02e5d8ffffffff0200651b90530000001976a914d5c7c9aec292a807005f013c4d2122f7126e257788ac00743ba40b0000001976a914211fd13b614521ed566ddd42738381e42c3c2b2088ac0000000001000000012908482e9f7d31e9dd392bb6e788a329458a3bc95230b468e4b8c578d27a63b3000000008a4730440220549a7b422fc2020671acabfb937349bd87d985b2e4b9698e4ccacc985f61aee102204dc272322079e9114746db2f8d035d82b64523a69cd7be674173e063090cc8ac014104011a6c220a5549ff112c92c6c38dec93f66ef1f0a21d1409b92f0ccf0fb159aa8173a5b2413a45140fc02b45d63775bae03691d9dc87fd7a10d709a04922900cffffffff0200743ba40b0000001976a914211fd13b614521ed566ddd42738381e42c3c2b2088ac00f1dfeb470000001976a9140adcb4e90cc87f53d7618294222a8a4e193ae9f088ac00000000");
-    }
+    private byte[] block700000Bytes;
+    private Block block700000;
 
     @Before
     public void setUp() throws Exception {
-        Context context = new Context(TESTNET2);
+        new Context(TESTNET);
+        // One with some of transactions in, so a good test of the merkle tree hashing.
+        block700000Bytes = ByteStreams.toByteArray(BlockTest.class.getResourceAsStream("block_testnet700000.dat"));
+        block700000 = TESTNET.getDefaultSerializer().makeBlock(block700000Bytes);
+        assertEquals("000000000000406178b12a4dea3b27e13b3c4fe4510994fd667d7c1e6a3f4dc1", block700000.getHashAsString());
     }
 
     @Test
     public void testWork() throws Exception {
-        BigInteger work = TESTNET2.getGenesisBlock().getWork();
+        BigInteger work = TESTNET.getGenesisBlock().getWork();
+        double log2Work = Math.log(work.longValue()) / Math.log(2);
         // This number is printed by Bitcoin Core at startup as the calculated value of chainWork on testnet:
-        //
-        // SetBestChain: new best=00000007199508e34a9f  height=0  work=536879104
-        assertEquals(BigInteger.valueOf(536879104L), work);
+        // UpdateTip: new best=000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943 height=0 version=0x00000001 log2_work=32.000022 tx=1 date='2011-02-02 23:16:42' ...
+        assertEquals(32.000022, log2Work, 0.0000001);
     }
 
     @Test
     public void testBlockVerification() throws Exception {
-        Block block = TESTNET2.getDefaultSerializer().makeBlock(blockBytes);
-        block.verify(Block.BLOCK_HEIGHT_GENESIS, EnumSet.noneOf(Block.VerifyFlag.class));
-        assertEquals("00000000a6e5eb79dcec11897af55e90cd571a4335383a3ccfbc12ec81085935", block.getHashAsString());
+        block700000.verify(Block.BLOCK_HEIGHT_GENESIS, EnumSet.noneOf(Block.VerifyFlag.class));
     }
     
     @SuppressWarnings("deprecation")
     @Test
     public void testDate() throws Exception {
-        Block block = TESTNET2.getDefaultSerializer().makeBlock(blockBytes);
-        assertEquals("4 Nov 2010 16:06:04 GMT", block.getTime().toGMTString());
+        assertEquals("2016-02-13T22:59:39Z", Utils.dateTimeFormat(block700000.getTime()));
     }
 
     @Test
     public void testProofOfWork() throws Exception {
         // This params accepts any difficulty target.
-        Block block = UNITTEST.getDefaultSerializer().makeBlock(blockBytes);
+        Block block = UNITTEST.getDefaultSerializer().makeBlock(block700000Bytes);
         block.setNonce(12346);
         try {
             block.verify(Block.BLOCK_HEIGHT_GENESIS, EnumSet.noneOf(Block.VerifyFlag.class));
@@ -110,19 +102,17 @@ public class BlockTest {
         // Should find an acceptable nonce.
         block.solve();
         block.verify(Block.BLOCK_HEIGHT_GENESIS, EnumSet.noneOf(Block.VerifyFlag.class));
-        assertEquals(block.getNonce(), 2);
     }
 
     @Test
     public void testBadTransactions() throws Exception {
-        Block block = TESTNET2.getDefaultSerializer().makeBlock(blockBytes);
         // Re-arrange so the coinbase transaction is not first.
-        Transaction tx1 = block.transactions.get(0);
-        Transaction tx2 = block.transactions.get(1);
-        block.transactions.set(0, tx2);
-        block.transactions.set(1, tx1);
+        Transaction tx1 = block700000.transactions.get(0);
+        Transaction tx2 = block700000.transactions.get(1);
+        block700000.transactions.set(0, tx2);
+        block700000.transactions.set(1, tx1);
         try {
-            block.verify(Block.BLOCK_HEIGHT_GENESIS, EnumSet.noneOf(Block.VerifyFlag.class));
+            block700000.verify(Block.BLOCK_HEIGHT_GENESIS, EnumSet.noneOf(Block.VerifyFlag.class));
             fail();
         } catch (VerificationException e) {
             // We should get here.
@@ -131,9 +121,8 @@ public class BlockTest {
 
     @Test
     public void testHeaderParse() throws Exception {
-        Block block = TESTNET2.getDefaultSerializer().makeBlock(blockBytes);
-        Block header = block.cloneAsHeader();
-        Block reparsed = TESTNET2.getDefaultSerializer().makeBlock(header.bitcoinSerialize());
+        Block header = block700000.cloneAsHeader();
+        Block reparsed = TESTNET.getDefaultSerializer().makeBlock(header.bitcoinSerialize());
         assertEquals(reparsed, header);
     }
 
@@ -143,8 +132,7 @@ public class BlockTest {
         // proves that transaction serialization works, along with all its subobjects like scripts and in/outpoints.
         //
         // NB: This tests the bitcoin serialization protocol.
-        Block block = TESTNET2.getDefaultSerializer().makeBlock(blockBytes);
-        assertTrue(Arrays.equals(blockBytes, block.bitcoinSerialize()));
+        assertTrue(Arrays.equals(block700000Bytes, block700000.bitcoinSerialize()));
     }
     
     @Test
@@ -185,7 +173,7 @@ public class BlockTest {
         // contains a coinbase transaction whose height is two bytes, which is
         // shorter than we see in most other cases.
 
-        Block block = TESTNET3.getDefaultSerializer().makeBlock(
+        Block block = TESTNET.getDefaultSerializer().makeBlock(
             ByteStreams.toByteArray(getClass().getResourceAsStream("block_testnet21066.dat")));
 
         // Check block.
@@ -197,7 +185,7 @@ public class BlockTest {
         // fit in two bytes. This test primarily ensures script encoding checks
         // are applied correctly.
 
-        block = TESTNET3.getDefaultSerializer().makeBlock(
+        block = TESTNET.getDefaultSerializer().makeBlock(
             ByteStreams.toByteArray(getClass().getResourceAsStream("block_testnet32768.dat")));
 
         // Check block.

dashpay/dashj

(cherry picked from commit b08cfe3f54711b605c939dbc3382225057bcfb9e)
Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

core/src/test/java/org/bitcoinj/core/BlockTest.java
@@ -21,7 +21,6 @@ import com.google.common.io.ByteStreams;
 
 import org.bitcoinj.core.AbstractBlockChain.NewBlockType;
 import org.bitcoinj.params.MainNetParams;
-import org.bitcoinj.params.TestNet2Params;
 import org.bitcoinj.params.TestNet3Params;
 import org.bitcoinj.params.UnitTestParams;
 import org.bitcoinj.script.ScriptOpCodes;
@@ -39,29 +38,25 @@ import static org.bitcoinj.core.Utils.HEX;
 import static org.junit.Assert.*;
 
 public class BlockTest {
-    private static final NetworkParameters TESTNET2 = TestNet2Params.get();
-    private static final NetworkParameters TESTNET3 = TestNet3Params.get();
+    private static final NetworkParameters TESTNET = TestNet3Params.get();
     private static final NetworkParameters UNITTEST = UnitTestParams.get();
     private static final NetworkParameters MAINNET = MainNetParams.get();
 
-    public static final byte[] blockBytes;
-
-    static {
-        // Block 000000000fba0622132b6acd887021db720c541590d0408bc3ae525277fb2636; height of 187943
-        // One with lots of transactions in, so a good test of the merkle tree hashing.
-        blockBytes = HEX.decode("00000020157dc13d40818f91eddb8d14dd2e8dcb43f2c4dd2935d4065238788d00000000ec269fd49d1d0c5613f918a686f71ddb3a8f92c7b85b385f3dfed4f0c5cd0a392b92f9584ee1001d059cf2910301000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4c0327de02042b92f95808fabe6d6dec5faa57bc58a0f6aeb6555f0900000092db0a8600000000ac88bd54c554c6e701000000000000006ffffff3710100000d2f6e6f64655374726174756d2f00000000022c460e43000000001976a914cf5783cdb4347cf4d6b4026fbdd895e460acbc5088ac2c460e43000000001976a91440312e1aeebc7e2e99047125d78f19f51df6180188ac0000000001000000032ec44449143edfba5bc741a899b22c9ea1797750195000cfc9a8317aca5b858a000000006b483045022100f04f5686febb8d9df0af2f8fee92e74048dd79ec6e44acb7c05a16b635c9320502207aed80d5b137463a6c0e5b2af859ce3e5475945ad167072f1216dccbd16ffee00121038ecdf3413d4e7e2797cc1b038f9ba9e42b83f261e64d9115569e46d741b2da24feffffffd9bb73a65adeeab77186fe0d33917a0661a6d6da7e94a64486fae91135adf71e000000006a473044022055ba9a7106dc28e90c72c2d41f937528ea6aacb371f79ca3a18f9305236b2c2d0220558d833028626f16ad93acfcb990ca68575d1874befc85a185ef78a3c11ccd5e0121038ecdf3413d4e7e2797cc1b038f9ba9e42b83f261e64d9115569e46d741b2da24feffffffdca428fda53beb894c01c398b7a92f744b60ae5c0e01ea29f985edf6305d322e000000006a47304402205edcea624377076f2db313346cc5a7f364594912cfd6a695e7beb77834c0ffc2022004d67d23b7c8b825d17c3f5a37cb57cee4fd5ff121c080b674d4d41decb52717012103353cc80209a47dbf0f434385bb293b1f08b92e2dd3917c51483e5c846ad77f71feffffff02c70a8d05000000001976a914c88b91c4e0e738d082f29fd784ff11de29eb244b88acf5741c86000000001976a914dc3e0793134b081145ec0c67a9c72a7b297df27c88ac25de0200010000000229035c9d173a3581cd3e751e30803d8007178debee5c43ab0667f22d3d1d85b5000000006a47304402205208c78c617e2847bde13b7d09c36b14aa048a55972f9c8b49f657ea82d1f5d602202fac89274602079cecd7bbe7cfd577e64084d68734531a56c2db0374f93ce0bb01210342d0a0181a33451fe901dec2a01ef22fc5ad570c5de507cfbf70417777fa3374feffffffdd2b4139ad014967454591a796045b2f02259def4d3c5a26c2c32e4f3d786f68000000006a47304402205f49906b990f0a35c5349de8453ebc7c70f3a59259cc524e1508c97a2eba09f00220745e5c0eb38421b9b7ac4e9c181bc3bb0d96b9c6eab26af9ea7ba5852664391e0121038ecdf3413d4e7e2797cc1b038f9ba9e42b83f261e64d9115569e46d741b2da24feffffff024b4a0f00000000001976a914f32788579f94cab7974f8119e7383c3d14357c5388ac40230e43000000001976a914dc3e0793134b081145ec0c67a9c72a7b297df27c88ac26de0200");
-    }
+    private byte[] blockBytes;
+    private Block block;
 
     @Before
     public void setUp() throws Exception {
-        Context context = new Context(TESTNET2);
+        new Context(TESTNET);
+        blockBytes = HEX.decode("00000020157dc13d40818f91eddb8d14dd2e8dcb43f2c4dd2935d4065238788d00000000ec269fd49d1d0c5613f918a686f71ddb3a8f92c7b85b385f3dfed4f0c5cd0a392b92f9584ee1001d059cf2910301000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4c0327de02042b92f95808fabe6d6dec5faa57bc58a0f6aeb6555f0900000092db0a8600000000ac88bd54c554c6e701000000000000006ffffff3710100000d2f6e6f64655374726174756d2f00000000022c460e43000000001976a914cf5783cdb4347cf4d6b4026fbdd895e460acbc5088ac2c460e43000000001976a91440312e1aeebc7e2e99047125d78f19f51df6180188ac0000000001000000032ec44449143edfba5bc741a899b22c9ea1797750195000cfc9a8317aca5b858a000000006b483045022100f04f5686febb8d9df0af2f8fee92e74048dd79ec6e44acb7c05a16b635c9320502207aed80d5b137463a6c0e5b2af859ce3e5475945ad167072f1216dccbd16ffee00121038ecdf3413d4e7e2797cc1b038f9ba9e42b83f261e64d9115569e46d741b2da24feffffffd9bb73a65adeeab77186fe0d33917a0661a6d6da7e94a64486fae91135adf71e000000006a473044022055ba9a7106dc28e90c72c2d41f937528ea6aacb371f79ca3a18f9305236b2c2d0220558d833028626f16ad93acfcb990ca68575d1874befc85a185ef78a3c11ccd5e0121038ecdf3413d4e7e2797cc1b038f9ba9e42b83f261e64d9115569e46d741b2da24feffffffdca428fda53beb894c01c398b7a92f744b60ae5c0e01ea29f985edf6305d322e000000006a47304402205edcea624377076f2db313346cc5a7f364594912cfd6a695e7beb77834c0ffc2022004d67d23b7c8b825d17c3f5a37cb57cee4fd5ff121c080b674d4d41decb52717012103353cc80209a47dbf0f434385bb293b1f08b92e2dd3917c51483e5c846ad77f71feffffff02c70a8d05000000001976a914c88b91c4e0e738d082f29fd784ff11de29eb244b88acf5741c86000000001976a914dc3e0793134b081145ec0c67a9c72a7b297df27c88ac25de0200010000000229035c9d173a3581cd3e751e30803d8007178debee5c43ab0667f22d3d1d85b5000000006a47304402205208c78c617e2847bde13b7d09c36b14aa048a55972f9c8b49f657ea82d1f5d602202fac89274602079cecd7bbe7cfd577e64084d68734531a56c2db0374f93ce0bb01210342d0a0181a33451fe901dec2a01ef22fc5ad570c5de507cfbf70417777fa3374feffffffdd2b4139ad014967454591a796045b2f02259def4d3c5a26c2c32e4f3d786f68000000006a47304402205f49906b990f0a35c5349de8453ebc7c70f3a59259cc524e1508c97a2eba09f00220745e5c0eb38421b9b7ac4e9c181bc3bb0d96b9c6eab26af9ea7ba5852664391e0121038ecdf3413d4e7e2797cc1b038f9ba9e42b83f261e64d9115569e46d741b2da24feffffff024b4a0f00000000001976a914f32788579f94cab7974f8119e7383c3d14357c5388ac40230e43000000001976a914dc3e0793134b081145ec0c67a9c72a7b297df27c88ac26de0200");
+        block = TESTNET.getDefaultSerializer().makeBlock(blockBytes);
+
     }
 
     @Test
     public void testWork() throws Exception {
-        BigInteger work = TESTNET2.getGenesisBlock().getWork();
+        BigInteger work = TESTNET.getGenesisBlock().getWork();
         // This number is printed by Dash Core at startup as the calculated value of chainWork on testnet:
-        //
         // 2017-04-22 19:20:20 UpdateTip: new best=00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c  height=0  log2_work=20.000022  tx=1  date=2014-01-25 16:10:06 progress=0.000002
         // log base 2 of 1048592 is 20.000022
         assertEquals(BigInteger.valueOf(1048592), work);
@@ -69,7 +64,7 @@ public class BlockTest {
 
     @Test
     public void testBlockVerification() throws Exception {
-        Block block = TESTNET2.getDefaultSerializer().makeBlock(blockBytes);
+        Block block = TESTNET.getDefaultSerializer().makeBlock(blockBytes);
         block.verify(Block.BLOCK_HEIGHT_GENESIS, EnumSet.noneOf(Block.VerifyFlag.class));
         assertEquals("000000000fba0622132b6acd887021db720c541590d0408bc3ae525277fb2636", block.getHashAsString());
     }
@@ -77,7 +72,7 @@ public class BlockTest {
     @SuppressWarnings("deprecation")
     @Test
     public void testDate() throws Exception {
-        Block block = TESTNET2.getDefaultSerializer().makeBlock(blockBytes);
+        Block block = TESTNET.getDefaultSerializer().makeBlock(blockBytes);
         assertEquals("21 Apr 2017 05:01:31 GMT", block.getTime().toGMTString());
     }
 
@@ -113,7 +108,6 @@ public class BlockTest {
 
     @Test
     public void testBadTransactions() throws Exception {
-        Block block = TESTNET2.getDefaultSerializer().makeBlock(blockBytes);
         // Re-arrange so the coinbase transaction is not first.
         Transaction tx1 = block.transactions.get(0);
         Transaction tx2 = block.transactions.get(1);
@@ -129,9 +123,8 @@ public class BlockTest {
 
     @Test
     public void testHeaderParse() throws Exception {
-        Block block = TESTNET2.getDefaultSerializer().makeBlock(blockBytes);
         Block header = block.cloneAsHeader();
-        Block reparsed = TESTNET2.getDefaultSerializer().makeBlock(header.bitcoinSerialize());
+        Block reparsed = TESTNET.getDefaultSerializer().makeBlock(header.bitcoinSerialize());
         assertEquals(reparsed, header);
     }
 
@@ -141,7 +134,6 @@ public class BlockTest {
         // proves that transaction serialization works, along with all its subobjects like scripts and in/outpoints.
         //
         // NB: This tests the bitcoin serialization protocol.
-        Block block = TESTNET2.getDefaultSerializer().makeBlock(blockBytes);
         assertTrue(Arrays.equals(blockBytes, block.bitcoinSerialize()));
     }
     
@@ -186,7 +178,6 @@ public class BlockTest {
         //Block block = TESTNET.getDefaultSerializer().makeBlock(
         //    ByteStreams.toByteArray(getClass().getResourceAsStream("block_testnet21066.dat")));
 
-        Block block = TestNet3Params.get().getDefaultSerializer().makeBlock(blockBytes);
         // Check block.
         assertEquals("000000000fba0622132b6acd887021db720c541590d0408bc3ae525277fb2636", block.getHashAsString());
         block.verify(187943, EnumSet.of(Block.VerifyFlag.HEIGHT_IN_COINBASE));
@@ -196,9 +187,9 @@ public class BlockTest {
         // fit in two bytes. This test primarily ensures script encoding checks
         // are applied correctly.
 
-        //block = TestNet3Params.get().getDefaultSerializer().makeBlock(
+        //block = TESTNET.getDefaultSerializer().makeBlock(
         //    ByteStreams.toByteArray(getClass().getResourceAsStream("block_testnet32768.dat")));
-        block = TESTNET3.getDefaultSerializer().makeBlock(HEX.decode("04000000ad7226a87e8c16a4de493320a6f55535903b13dae0689bc6a76255ad0400000068208963dae191aa15c11dd82adfd7ba7184716b9acc649805904ec9a9d564079a3036578798311d3909b8000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff050288130101ffffffff0100743ba40b0000002321033de1afba2a2aa9f88f06912a4b51d386af3142e89f3b1a69741cb62f3dd47cdaac00000000"));
+        block = TESTNET.getDefaultSerializer().makeBlock(HEX.decode("04000000ad7226a87e8c16a4de493320a6f55535903b13dae0689bc6a76255ad0400000068208963dae191aa15c11dd82adfd7ba7184716b9acc649805904ec9a9d564079a3036578798311d3909b8000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff050288130101ffffffff0100743ba40b0000002321033de1afba2a2aa9f88f06912a4b51d386af3142e89f3b1a69741cb62f3dd47cdaac00000000"));
         // Check block.
         assertEquals("0000001d3f1f66dd3b276f7fddaae344973e9ddf1179d904b0798bcfc503cd94", block.getHashAsString());
         block.verify(5000, EnumSet.of(Block.VerifyFlag.HEIGHT_IN_COINBASE));
@@ -297,13 +288,13 @@ public class BlockTest {
         String block842284hash = "000000000000000cdf5cc24c3beb0669b31e942d1301e07b53d6f0c7db10860d";
 
         Sha256Hash firstBlockHash = Sha256Hash.wrap(block842285hash);
-        Block firstBlock = new Block(TESTNET3, Utils.HEX.decode(block842285));
+        Block firstBlock = new Block(TESTNET, Utils.HEX.decode(block842285));
         assertEquals(firstBlockHash, firstBlock.getHash());
         assertEquals(12, firstBlock.getTransactions().size());
         assertEquals(1, firstBlock.getTransactions().get(0).getVersion());
 
         Sha256Hash secondBlockHash = Sha256Hash.wrap(block842284hash);
-        Block secondBlock = new Block(TESTNET3, Utils.HEX.decode(block842284));
+        Block secondBlock = new Block(TESTNET, Utils.HEX.decode(block842284));
         assertEquals(secondBlockHash, secondBlock.getHash());
         assertEquals(14, secondBlock.getTransactions().size());
 