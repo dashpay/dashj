bitcoinj/bitcoinj

TransactionOutput: Deprecate getAddressFromP2PKHScript() and getAddressFromP2SH() methods.

core/src/main/java/org/bitcoinj/core/TransactionOutput.java
@@ -119,40 +119,20 @@ public class TransactionOutput extends ChildMessage {
         return scriptPubKey;
     }
 
-    /**
-     * <p>If the output script pays to an address as in <a href="https://bitcoin.org/en/developer-guide#term-p2pkh">
-     * P2PKH</a>, return the address of the receiver, i.e., a base58 encoded hash of the public key in the script. </p>
-     *
-     * @param networkParameters needed to specify an address
-     * @return null, if the output script is not the form <i>OP_DUP OP_HASH160 <PubkeyHash> OP_EQUALVERIFY OP_CHECKSIG</i>,
-     * i.e., not P2PKH
-     * @return an address made out of the public key hash
-     */
     @Nullable
-    public LegacyAddress getAddressFromP2PKHScript(NetworkParameters networkParameters) throws ScriptException{
+    @Deprecated
+    public LegacyAddress getAddressFromP2PKHScript(NetworkParameters params) throws ScriptException {
         if (ScriptPattern.isPayToPubKeyHash(getScriptPubKey()))
-            return (LegacyAddress) getScriptPubKey().getToAddress(networkParameters);
-
+            return LegacyAddress.fromPubKeyHash(params,
+                    ScriptPattern.extractHashFromPayToPubKeyHash(getScriptPubKey()));
         return null;
     }
 
-    /**
-     * <p>If the output script pays to a redeem script, return the address of the redeem script as described by,
-     * i.e., a base58 encoding of [one-byte version][20-byte hash][4-byte checksum], where the 20-byte hash refers to
-     * the redeem script.</p>
-     *
-     * <p>P2SH is described by <a href="https://github.com/bitcoin/bips/blob/master/bip-0016.mediawiki">BIP 16</a> and
-     * <a href="https://bitcoin.org/en/developer-guide#p2sh-scripts">documented in the Bitcoin Developer Guide</a>.</p>
-     *
-     * @param networkParameters needed to specify an address
-     * @return null if the output script does not pay to a script hash
-     * @return an address that belongs to the redeem script
-     */
     @Nullable
-    public LegacyAddress getAddressFromP2SH(NetworkParameters networkParameters) throws ScriptException{
+    @Deprecated
+    public LegacyAddress getAddressFromP2SH(NetworkParameters params) throws ScriptException {
         if (ScriptPattern.isPayToScriptHash(getScriptPubKey()))
-            return (LegacyAddress) getScriptPubKey().getToAddress(networkParameters);
-
+            return LegacyAddress.fromP2SHHash(params, ScriptPattern.extractHashFromPayToScriptHash(getScriptPubKey()));
         return null;
     }
 

dashpay/dashj

(cherry picked from commit bc8828c8585cfd3ce581d6fbbc8a37d86776fbba)
Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

core/src/main/java/org/bitcoinj/core/TransactionOutput.java
@@ -119,40 +119,20 @@ public class TransactionOutput extends ChildMessage {
         return scriptPubKey;
     }
 
-    /**
-     * <p>If the output script pays to an address as in <a href="https://bitcoin.org/en/developer-guide#term-p2pkh">
-     * P2PKH</a>, return the address of the receiver, i.e., a base58 encoded hash of the public key in the script. </p>
-     *
-     * @param networkParameters needed to specify an address
-     * @return null, if the output script is not the form <i>OP_DUP OP_HASH160 <PubkeyHash> OP_EQUALVERIFY OP_CHECKSIG</i>,
-     * i.e., not P2PKH
-     * @return an address made out of the public key hash
-     */
     @Nullable
-    public Address getAddressFromP2PKHScript(NetworkParameters networkParameters) throws ScriptException{
+    @Deprecated
+    public Address getAddressFromP2PKHScript(NetworkParameters params) throws ScriptException {
         if (ScriptPattern.isPayToPubKeyHash(getScriptPubKey()))
-            return getScriptPubKey().getToAddress(networkParameters);
-
+            return Address.fromPubKeyHash(params,
+                    ScriptPattern.extractHashFromPayToPubKeyHash(getScriptPubKey()));
         return null;
     }
 
-    /**
-     * <p>If the output script pays to a redeem script, return the address of the redeem script as described by,
-     * i.e., a base58 encoding of [one-byte version][20-byte hash][4-byte checksum], where the 20-byte hash refers to
-     * the redeem script.</p>
-     *
-     * <p>P2SH is described by <a href="https://github.com/bitcoin/bips/blob/master/bip-0016.mediawiki">BIP 16</a> and
-     * <a href="https://bitcoin.org/en/developer-guide#p2sh-scripts">documented in the Bitcoin Developer Guide</a>.</p>
-     *
-     * @param networkParameters needed to specify an address
-     * @return null if the output script does not pay to a script hash
-     * @return an address that belongs to the redeem script
-     */
     @Nullable
-    public Address getAddressFromP2SH(NetworkParameters networkParameters) throws ScriptException{
+    @Deprecated
+    public Address getAddressFromP2SH(NetworkParameters params) throws ScriptException {
         if (ScriptPattern.isPayToScriptHash(getScriptPubKey()))
-            return getScriptPubKey().getToAddress(networkParameters);
-
+            return Address.fromP2SHHash(params, ScriptPattern.extractHashFromPayToScriptHash(getScriptPubKey()));
         return null;
     }
 