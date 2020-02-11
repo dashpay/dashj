5665a485920216d7e65c45668f607c8d8d83a6e6
HashEngineering <hashengineeringsolutions@gmail.com>
2020-02-10 18:19:38 -0800

    DevNetParams:  Add an add method, fix get method

--- core/src/main/java/org/bitcoinj/params/DevNetParams.java
+++ core/src/main/java/org/bitcoinj/params/DevNetParams.java
@@ -1,6 +1,5 @@
 /*
- * Copyright 2013 Google Inc.
- * Copyright 2014 Andreas Schildbach
+ * Copyright 2020 Dash Core Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -153,11 +152,21 @@ public class DevNetParams extends AbstractBitcoinNetParams {
     }
 
     public static synchronized DevNetParams get(String devNetName) {
+        if (instances == null) {
+            instances = new HashMap<>(1);
+        }
+
         if(!instances.containsKey("devnet-" + devNetName)) {
             return null;
         } else {
             return instances.get("devnet-" + devNetName);
         }
+    }
+
+    public static synchronized void add(DevNetParams params) {
+        if (instances == null)
+            instances = new HashMap<>(1);
+        instances.put(params.devNetName, params);
     }
 
     @Override
