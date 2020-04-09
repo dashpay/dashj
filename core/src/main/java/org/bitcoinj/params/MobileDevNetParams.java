d5b19e4ce8dacab77c602e6a3a0cdf1001300fb9
HashEngineering <hashengineeringsolutions@gmail.com>
2020-04-09 08:08:47 -0700

    MobileDevNetParams:  Update masternode list

--- core/src/main/java/org/bitcoinj/params/MobileDevNetParams.java
+++ core/src/main/java/org/bitcoinj/params/MobileDevNetParams.java
@@ -20,17 +20,29 @@ public class MobileDevNetParams extends DevNetParams {
 
     private static String DEVNET_NAME = "mobile";
 
-    private static String [] MASTERNODES = new String [] {
-            "18.237.82.208", "54.149.106.153", "54.202.230.77",
-            "18.237.204.156", "54.186.232.69", "54.218.124.137",
-            "52.32.58.19", "54.212.228.74","34.219.23.213",
-            "34.222.212.20", "34.220.26.32", "34.209.79.74",
-            "35.165.164.201",
+    public static String [] DNS_SEEDERS = new String [] {
+            "seed.mobile.networks.dash.org",
+    };
+
+    public static String [] MASTERNODES = new String [] {
+            "34.222.170.127",
+            "54.184.198.68",
+            "34.216.139.17",
+            "54.218.124.42",
+            "52.26.228.25",
+            "34.213.26.85",
+            "34.219.38.82",
+            "34.217.61.209",
+            "54.184.183.120",
+            "54.202.130.163",
+            "54.214.177.132",
+            "54.212.91.206",
+            "34.219.148.253",
     };
 
     public MobileDevNetParams() {
         super(DEVNET_NAME, "yQuAu9YAMt4yEiXBeDp3q5bKpo7jsC2eEj", 20001,
-                MASTERNODES, true, 70216);
+                DNS_SEEDERS, true, 70216);
     }
 
     private static MobileDevNetParams instance;
