950eac655d15f98098cd8c373cb1903d19fded73
HashEngineering <hashengineeringsolutions@gmail.com>
2020-07-21 10:16:09 -0700

    PublicKey: Update code, format, copyright
    (cherry picked from commit f924441e2489ae9957f2ada388cc00f3482d7e15)

--- core/src/main/java/org/bitcoinj/core/PublicKey.java
+++ core/src/main/java/org/bitcoinj/core/PublicKey.java
@@ -1,63 +1,46 @@
+/*
+ * Copyright 2015 Hash Engineering Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.bitcoinj.core;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.google.common.primitives.Ints;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.math.BigInteger;
-import java.security.SignatureException;
 import java.util.Arrays;
 
 /**
  * Created by Hash Engineering on 2/10/2015.
+ *
+ * PublicKey holds an ECDSA public key
  */
 public class PublicKey extends ChildMessage {
-    private static final Logger log = LoggerFactory.getLogger(PublicKey.class);
 
-    byte [] bytes;
-    ECKey key;
-
-    public PublicKey()
-    {
-        bytes = new byte[1];
-    }
-
-    PublicKey(NetworkParameters params)
-    {
-        super(params);
-    }
-    void invalidate()
-    {
-        bytes[0] = (byte)0xFF;
-    }
+    private byte [] bytes;
+    private ECKey key;
 
     public PublicKey(NetworkParameters params, byte[] payload, int offset) throws ProtocolException {
         super(params, payload, offset);
     }
 
-    /*public PublicKey(NetworkParameters params, byte[] payloadBytes, int cursor, Message parent, boolean parseLazy, boolean parseRetain)
-    {
-        super(params, payloadBytes, cursor, parent, parseLazy, parseRetain, payloadBytes.length);
-    }*/
     public PublicKey(byte [] key)
     {
         super();
         bytes = new byte[key.length];
         System.arraycopy(key, 0, bytes, 0, key.length);
         this.key = ECKey.fromPublicOnly(bytes);
-    }
-
-    protected static int calcLength(byte[] buf, int offset) {
-        VarInt varint;
-
-        int cursor = offset;// + 4;
-        varint = new VarInt(buf, cursor);
-        long len = varint.value;
-        len += varint.getOriginalSizeInBytes();
-        cursor += len;
-
-        return cursor - offset;
     }
 
     public int calculateMessageSizeInBytes()
@@ -67,81 +50,49 @@ public class PublicKey extends ChildMessage {
 
     @Override
     protected void parse() throws ProtocolException {
-
         cursor = offset;
-
         bytes = readByteArray();
-        //this.key = ECKey.fromPublicOnly(bytes);
-
         length = cursor - offset;
     }
+
     @Override
     protected void bitcoinSerializeToStream(OutputStream stream) throws IOException {
-
-        stream.write(new VarInt(bytes.length).encode());
-        stream.write(bytes);
+        Utils.bytesToByteStream(bytes, stream);
     }
 
     public String toString()
     {
-        return "public key:  " + Utils.HEX.encode(bytes);
-
+        return "PublicKey(" + Utils.HEX.encode(bytes) +")";
     }
 
     public byte [] getBytes() { return bytes; }
 
     public boolean equals(Object o)
     {
-       PublicKey key = (PublicKey)o;
-        if(key.bytes.length == this.bytes.length)
-        {
-            if(Arrays.equals(key.bytes, this.bytes) == true)
-                return true;
-        }
-        return false;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PublicKey key = (PublicKey)o;
+        return Arrays.equals(key.bytes, this.bytes);
     }
 
-    @Deprecated
-    PublicKey duplicate()
-    {
-        PublicKey copy = new PublicKey(getBytes());
-
-        return copy;
-    }
-
-    //
-    //  This doesn't work.  May not be necessary;  ECKey.verifyMessage handles making a ECDSASignature from the signature
-    //
-    static public PublicKey recoverCompact(Sha256Hash hash, MasternodeSignature sig) throws SignatureException
-    {
-        if(sig.getBytes().length != 65)
-            throw new SignatureException("signature is wrong size");
-        int recid = (sig.getBytes()[0] - 27) & 3;
-        boolean comp = ((sig.getBytes()[0] - 27) & 4) != 0;
-
-        //ECKey.ECDSASignature esig = ECKey.ECDSASignature.decodeFromDER(sig.getBytes());
-        BigInteger r = new BigInteger(1, Arrays.copyOfRange(sig.getBytes(), 1, 33));
-        BigInteger s = new BigInteger(1, Arrays.copyOfRange(sig.getBytes(), 33, 65));
-        ECKey.ECDSASignature esig = new ECKey.ECDSASignature(r, s);
-        ECKey ecKey = ECKey.recoverFromSignature(recid, esig, hash, comp);
-
-
-
-        return new PublicKey (ecKey.getPubKey());
+    /**
+     * Returns the first four bytes of the public key.
+     */
+    @Override
+    public int hashCode() {
+        return Ints.fromBytes(bytes[0], bytes[1], bytes[2], bytes[3]);
     }
 
     public byte [] getId()
     {
-        return getECKey().getPubKeyHash();
+        return getKey().getPubKeyHash();
     }
 
-    public ECKey getECKey()
+    public ECKey getKey()
     {
         if(key == null) {
             key = ECKey.fromPublicOnly(bytes);
         }
         return key;
-
     }
-
 }
