3f134a521f8280ed2607609206d4a44f08987e0f
HashEngineering <hashengineeringsolutions@gmail.com>
2020-09-17 07:40:31 -0700

    Allow peerGroup to be null for tests
    Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

--- core/src/main/java/org/bitcoinj/evolution/SimplifiedMasternodeListManager.java
+++ core/src/main/java/org/bitcoinj/evolution/SimplifiedMasternodeListManager.java
@@ -17,6 +17,7 @@ import org.bitcoinj.quorums.SimplifiedQuorumList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.CancelledKeyException;
@@ -409,6 +410,9 @@ public class SimplifiedMasternodeListManager extends AbstractManager {
     ReorganizeListener reorganizeListener = new ReorganizeListener() {
         @Override
         public void reorganize(StoredBlock splitPoint, List<StoredBlock> oldBlocks, List<StoredBlock> newBlocks) throws VerificationException {
+            if (!shouldProcessMNListDiff()) {
+                return;
+            }
             lock.lock();
             try {
                 SimplifiedMasternodeList mnlistAtSplitPoint = mnListsCache.get(splitPoint.getHeader().getHash());
@@ -622,16 +626,18 @@ public class SimplifiedMasternodeListManager extends AbstractManager {
 
     }
 
-    public void setBlockChain(AbstractBlockChain blockChain, AbstractBlockChain headersChain, PeerGroup peerGroup) {
+    public void setBlockChain(AbstractBlockChain blockChain, AbstractBlockChain headersChain, @Nullable PeerGroup peerGroup) {
         this.blockChain = blockChain;
         this.peerGroup = peerGroup;
         this.headersChain = headersChain;
         if(shouldProcessMNListDiff()) {
             blockChain.addNewBestBlockListener(Threading.SAME_THREAD, newBestBlockListener);
             blockChain.addReorganizeListener(reorganizeListener);
-            peerGroup.addConnectedEventListener(peerConnectedEventListener);
-            peerGroup.addChainDownloadStartedEventListener(chainDownloadStartedEventListener);
-            peerGroup.addDisconnectedEventListener(peerDisconnectedEventListener);
+            if (peerGroup != null) {
+                peerGroup.addConnectedEventListener(peerConnectedEventListener);
+                peerGroup.addChainDownloadStartedEventListener(chainDownloadStartedEventListener);
+                peerGroup.addDisconnectedEventListener(peerDisconnectedEventListener);
+            }
         }
     }
 
