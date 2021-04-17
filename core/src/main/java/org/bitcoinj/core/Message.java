58ff2d9f9b336860a1f7f48e79f9fde784c5fc46
Andreas Schildbach <andreas.schildbach@bloq.com>
2021-04-17 17:17:47 +0200

    Message: Fix exception due to unknown message having an unknown length. That's ok.
    (cherry picked from commit 063820f4ed0fed65a4341b159a073ac230ad2fee)

--- core/src/main/java/org/bitcoinj/core/Message.java
+++ core/src/main/java/org/bitcoinj/core/Message.java
@@ -92,7 +92,7 @@ public abstract class Message {
 
         parse();
 
-        if (this.length == UNKNOWN_LENGTH)
+        if (this.length == UNKNOWN_LENGTH && !(this instanceof UnknownMessage))
             checkState(false, "Length field has not been set in constructor for %s after parse.",
                        getClass().getSimpleName());
 
