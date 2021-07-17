c05324ba14cf3a7c06bcbf57f7b8d34c7bfbb789
HashEngineering <hashengineeringsolutions@gmail.com>
2021-07-16 18:37:48 -0700

    Quorum: add get methods

--- core/src/main/java/org/bitcoinj/quorums/Quorum.java
+++ core/src/main/java/org/bitcoinj/quorums/Quorum.java
@@ -1,5 +1,7 @@
 package org.bitcoinj.quorums;
 
+import org.bitcoinj.core.Sha256Hash;
+
 public class Quorum {
 
     LLMQParameters llmqParameters;
@@ -13,5 +15,17 @@ public class Quorum {
     @Override
     public String toString() {
         return String.format("Quorum(type=%d, quorumHash=%s, qfc=%s)", llmqParameters.type.value, commitment.quorumHash, commitment);
+    }
+
+    public FinalCommitment getCommitment() {
+        return commitment;
+    }
+
+    public LLMQParameters getLlmqParameters() {
+        return llmqParameters;
+    }
+
+    public Sha256Hash getQuorumHash() {
+        return commitment.quorumHash;
     }
 }
