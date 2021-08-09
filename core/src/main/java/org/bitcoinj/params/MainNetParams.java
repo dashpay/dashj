0630686710afe56cf3055797153005c1f50a45bc
HashEngineering <hashengineeringsolutions@gmail.com>
2021-08-09 14:04:02 -0700

    fix: mainnet dip14 public version bytes

--- core/src/main/java/org/bitcoinj/params/MainNetParams.java
+++ core/src/main/java/org/bitcoinj/params/MainNetParams.java
@@ -52,7 +52,7 @@ public class MainNetParams extends AbstractBitcoinNetParams {
         packetMagic = CoinDefinition.PacketMagic;
         bip32HeaderP2PKHpub = 0x0488b21e; // The 4 byte header that serializes in base58 to "xpub".
         bip32HeaderP2PKHpriv = 0x0488ade4; // The 4 byte header that serializes in base58 to "xprv"
-        dip14HeaderP2PKHpub = 0x0eecefc6; // The 4 byte header that serializes in base58 to "dpmp".
+        dip14HeaderP2PKHpub = 0x0eecefc5; // The 4 byte header that serializes in base58 to "dpmp".
         dip14HeaderP2PKHpriv = 0x0eecf02e; // The 4 byte header that serializes in base58 to "dpms"
 
         genesisBlock.setDifficultyTarget(CoinDefinition.genesisBlockDifficultyTarget);
