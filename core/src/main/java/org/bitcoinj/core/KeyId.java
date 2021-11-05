d1e4330553b435ff3659272bcbef00c3560ceccc
HashEngineering <hashengineeringsolutions@gmail.com>
2021-11-04 20:54:23 -0700

    chore: remove duplicate hashCode method

--- core/src/main/java/org/bitcoinj/core/KeyId.java
+++ core/src/main/java/org/bitcoinj/core/KeyId.java
@@ -76,11 +76,6 @@ public class KeyId extends ChildMessage {
         return Arrays.equals(keyId.bytes, this.bytes);
     }
 
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(bytes);
-    }
-
     Address getAddress(NetworkParameters params) {
         return Address.fromScriptHash(params, bytes);
     }
