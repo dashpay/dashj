5132641d1a72c28dfa5a2b94173c16b3d7451fa5
HashEngineering <hashengineeringsolutions@gmail.com>
2021-11-15 18:41:38 -0800

    feat: allow using a lower protocol version to appear as Dash Core 0.17 client
    Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

--- core/src/main/java/org/bitcoinj/core/NetworkParameters.java
+++ core/src/main/java/org/bitcoinj/core/NetworkParameters.java
@@ -649,6 +649,7 @@ public abstract class NetworkParameters {
         BLOOM_FILTER_BIP111(MINIMUM.getBitcoinProtocolVersion()+1),
         @Deprecated
         DMN_LIST(70214),
+        CORE17(70219),
         ISDLOCK(70220),
         CURRENT(70220);
 
@@ -671,7 +672,7 @@ public abstract class NetworkParameters {
     protected int superblockStartBlock;
     protected int superblockCycle; // in blocks
 
-    protected boolean supportsEvolution = true;
+    protected boolean supportsV18 = true;
 
     /**
      * Getter for property 'nGovernanceMinQuorum'.
@@ -703,8 +704,12 @@ public abstract class NetworkParameters {
         return superblockStartBlock;
     }
 
-    public boolean isSupportingEvolution() {
-        return supportsEvolution;
+    public boolean isSupportingV18() {
+        return supportsV18;
+    }
+
+    public void setSupportsV18(boolean supportsV18) {
+        this.supportsV18 = supportsV18;
     }
 
     protected int instantSendConfirmationsRequired;
