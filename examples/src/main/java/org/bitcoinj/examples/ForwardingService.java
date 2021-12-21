9763f1c03e884eb00f36f6b5b436364fc51863c5
HashEngineering <hashengineeringsolutions@gmail.com>
2021-12-21 10:23:24 -0800

    feat: add malort devent support to ForwardingService
    Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

--- examples/src/main/java/org/bitcoinj/examples/ForwardingService.java
+++ examples/src/main/java/org/bitcoinj/examples/ForwardingService.java
@@ -70,6 +70,9 @@ public class ForwardingService {
         } else if (args.length > 1 && args[1].equals("schnapps")) {
             params = SchnappsDevNetParams.get();
             filePrefix = "forwarding-service-schnapps";
+        } else if (args.length > 1 && args[1].equals("malort")) {
+            params = MalortDevNetParams.get();
+            filePrefix = "forwarding-service-malort";
         } else if( args.length > 6 && args[1].equals("devnet")) {
             String [] dnsSeeds = new String[args.length - 5];
             System.arraycopy(args, 5, dnsSeeds, 0, args.length - 5);
