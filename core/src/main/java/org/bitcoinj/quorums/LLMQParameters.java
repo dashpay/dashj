214e34b9c4b4e0e077e2315b67eea4d6910890d6
HashEngineering <hashengineeringsolutions@gmail.com>
2022-08-02 21:15:06 -0700

    fix: update LLMQParameters for some quorum types
    Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

--- core/src/main/java/org/bitcoinj/quorums/LLMQParameters.java
+++ core/src/main/java/org/bitcoinj/quorums/LLMQParameters.java
@@ -100,7 +100,7 @@ public class LLMQParameters {
     static {
         availableLlmqs = new HashMap<>(7);
         availableLlmqs.put(LLMQType.LLMQ_TEST, new LLMQParameters(LLMQType.LLMQ_TEST, "llmq_test",
-                4, 2, 2, 24, 2, 10,
+                3, 2, 2, 24, 2, 10,
                 18, 2, 2, 3, 3));
         availableLlmqs.put(LLMQType.LLMQ_TEST_V17, new LLMQParameters(LLMQType.LLMQ_TEST_V17, "llmq_test_v17",
                 3, 2, 2, 24, 2, 10,
@@ -108,11 +108,11 @@ public class LLMQParameters {
 
         availableLlmqs.put(LLMQType.LLMQ_DEVNET, new LLMQParameters(LLMQType.LLMQ_DEVNET, "llmq_devnet",
                 12, 7, 6, 24, 2, 10,
-                18, 7, 4, 4, 6));
+                18, 7, 4, 5, 6));
 
         availableLlmqs.put(LLMQType.LLMQ_DEVNET_DIP0024, new LLMQParameters(LLMQType.LLMQ_DEVNET_DIP0024, "llmq_devnet_2",
-                8, 6, 4, 48, 2, 10,
-                18, 7, 2, 4, 4));
+                8, 6, 4, 48, 2, 12,
+                20, 7, 2, 4, 4));
 
         availableLlmqs.put(LLMQType.LLMQ_50_60, new LLMQParameters(LLMQType.LLMQ_50_60, "llmq_50_60",
                 50, 40, 30, 24, 2, 10,
@@ -120,7 +120,7 @@ public class LLMQParameters {
 
         availableLlmqs.put(LLMQType.LLMQ_400_60, new LLMQParameters(LLMQType.LLMQ_400_60, "llmq_400_60",
                 400, 300, 240, 24*12, 4, 20,
-                28, 300, 4, 5, 100));
+                48, 300, 4, 5, 100));
 
         availableLlmqs.put(LLMQType.LLMQ_400_85, new LLMQParameters(LLMQType.LLMQ_400_85, "llmq_400_85",
                 400, 350, 340, 24 * 24, 4, 20,
@@ -131,16 +131,16 @@ public class LLMQParameters {
                 18, 80, 24, 25, 50));
 
         availableLlmqs.put(LLMQType.LLMQ_TEST_DIP0024, new LLMQParameters(LLMQType.LLMQ_TEST_DIP0024, "llmq_test_dip0024",
-                4, 3, 2, 24, 2, 10,
-                18, 2, 2, 3, 3));
+                4, 3, 2, 24, 2, 12,
+                20, 2, 2, 3, 3));
 
         availableLlmqs.put(LLMQType.LLMQ_TEST_INSTANTSEND, new LLMQParameters(LLMQType.LLMQ_TEST_INSTANTSEND, "llmq_test_instantsend",
                 3, 2, 2, 24, 2, 10,
-                18, 2, 2, 3, 3));
+                18, 2, 2, 4, 3));
 
         availableLlmqs.put(LLMQType.LLMQ_60_75, new LLMQParameters(LLMQType.LLMQ_60_75, "llmq_60_75",
-                60, 50, 45, 24 * 2, 4, 20,
-                28, 48, 16, 64, 25));
+                60, 50, 45, 24 * 12, 2, 42,
+                50, 48, 32, 64, 25));
 
     }
 
