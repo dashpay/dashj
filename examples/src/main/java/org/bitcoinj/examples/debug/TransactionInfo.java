b309be87f3c916cca04e3c99c8c053df38c3e37e
HashEngineering <hashengineeringsolutions@gmail.com>
2022-08-08 21:44:45 -0700

    feat: add debugging support for transaction monitoring

---
+++ examples/src/main/java/org/bitcoinj/examples/debug/TransactionInfo.java
@@ -0,0 +1,33 @@
+package org.bitcoinj.examples.debug;
+
+import org.bitcoinj.core.Transaction;
+import org.json.JSONObject;
+
+public class TransactionInfo {
+    long timeReceived;
+    int blockRecieved;
+    Transaction tx;
+    JSONObject txCore;
+
+    TransactionInfo(long timeReceived, int blockRecieved, Transaction tx) {
+        this.timeReceived = timeReceived;
+        this.blockRecieved = blockRecieved;
+        this.tx = tx;
+    }
+
+    @Override
+    public int hashCode() {
+        return tx.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TransactionInfo that = (TransactionInfo) o;
+
+        if (timeReceived != that.timeReceived) return false;
+        return tx != null ? tx.equals(that.tx) : that.tx == null;
+    }
+}
---
+++ examples/src/main/java/org/bitcoinj/examples/debug/TransactionReport.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2022 Dash Core Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.examples.debug;
+
+import org.bitcoinj.core.Sha256Hash;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.TransactionConfidence;
+import org.bitcoinj.quorums.LLMQParameters;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TransactionReport {
+
+    static Logger log = LoggerFactory.getLogger(TransactionReport.class);
+    ArrayList<TransactionInfo> txList = new ArrayList<>();
+    HashMap<Sha256Hash, TransactionInfo> txMap = new HashMap<>();
+
+    File outputFile = new File("tx-report.csv");
+    String dashClientPath;
+    String confPath;
+
+    public TransactionReport(String dashClientPath, String confPath) {
+        this.dashClientPath = dashClientPath;
+        this.confPath = confPath;
+    }
+
+    public void add(long timestamp, int blockReceived, Transaction tx) {
+        TransactionInfo txInfo = new TransactionInfo(timestamp, blockReceived, tx);
+
+        txList.add(txInfo);
+        txMap.put(tx.getTxId(), txInfo);
+    }
+
+    JSONObject runRPCCommand(String command) {
+        try {
+            String config;
+            if (confPath.startsWith("-")) {
+                config = confPath;
+            } else {
+                config = String.format("-conf=%s", confPath);
+            }
+            Process process = Runtime.getRuntime().exec(String.format("%s %s %s", dashClientPath, config, command));
+            int result = process.waitFor();
+
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(process.getInputStream()));
+
+            String s;
+            StringBuilder output = new StringBuilder();
+            while ((s = stdInput.readLine()) != null) {
+                output.append(s);
+            }
+
+            return new JSONObject(output.toString());
+
+        } catch (IOException e) {
+            return null;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void printReport() {
+        try {
+            FileWriter writer = new FileWriter(outputFile);
+            int cycleLength = -1;
+            writer.append("TxId, Block Received, Block Mined, In Mempool, IS Status, Core instantlock_internal, Block Rec. Mod, Cycle Hash, Quorum Hash:Index\n");
+            for (TransactionInfo txInfo : txList) {
+                if (cycleLength == -1) {
+                    cycleLength = LLMQParameters.fromType(txInfo.tx.getParams().getLlmqDIP0024InstantSend()).getDkgInterval();
+                }
+                TransactionConfidence confidence = txInfo.tx.getConfidence();
+                int blockMined = confidence.getConfidenceType() == TransactionConfidence.ConfidenceType.BUILDING ? confidence.getAppearedAtChainHeight() : -1;
+
+                if ((txInfo.txCore == null || blockMined == -1) && dashClientPath.length() > 0) {
+                    txInfo.txCore = runRPCCommand(String.format("getrawtransaction %s true", txInfo.tx.getTxId()));
+                }
+                String line = String.format("%s, %d, %d, %d, %s, %s, %d, %s, %s\n",
+                        txInfo.tx.getTxId(),
+                        txInfo.blockRecieved,
+                        blockMined,
+                        blockMined != -1 ? blockMined - txInfo.blockRecieved : 0,
+                        confidence.getIXType(),
+                        txInfo.txCore != null && txInfo.txCore.getBoolean("instantlock_internal"),
+                        txInfo.blockRecieved % cycleLength,
+                        confidence.getIXType() != TransactionConfidence.IXType.IX_NONE && confidence.getInstantSendlock() != null ? confidence.getInstantSendlock().getCycleHash() : "",
+                        (confidence.getIXType() != TransactionConfidence.IXType.IX_NONE && confidence.getIXType() != TransactionConfidence.IXType.IX_REQUEST && confidence.getInstantSendlock() != null) ? (confidence.getInstantSendlock().getQuorumHash() + ":" + confidence.getInstantSendlock().getQuorumIndex()):"");
+                writer.append(line);
+            }
+            writer.close();
+        } catch (FileNotFoundException e) {
+            log.error("file not found", e);
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            log.error("io error", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
