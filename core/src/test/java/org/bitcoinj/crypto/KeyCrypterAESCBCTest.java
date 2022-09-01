0b80e741524a833e31b48576755c573f29525da1
HashEngineering <hashengineeringsolutions@gmail.com>
2022-08-31 17:30:12 -0700

    feat: add EC and BLS key support to KeyCrypterAESCBC
    * add a test that uses a EC private key for AES

---
+++ core/src/test/java/org/bitcoinj/crypto/KeyCrypterAESCBCTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2013 Jim Burton.
+ * Copyright 2014 Andreas Schildbach
+ *
+ * Licensed under the MIT license (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://opensource.org/licenses/mit-license.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.crypto;
+
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.Utils;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class KeyCrypterAESCBCTest {
+
+    ECKey aliceKey;
+
+    static String secret = "my little secret is a pony that never sleeps";
+
+    @Before
+    public void setup() {
+        aliceKey = ECKey.fromPrivate(Utils.HEX.decode("7337920e97ebfe34ee82137a26ef7bd296a3132f3c6494235e1f17820770aa01"));
+    }
+
+    @Test
+    public void testEncryptionAndDecryption() {
+        //Alice is sending to Bob
+        KeyCrypterECDH aliceKeyExchangeCrypter = new KeyCrypterECDH();
+        KeyParameter aliceKeyParameter = aliceKeyExchangeCrypter.deriveKey(aliceKey);
+
+        EncryptedData encryptedData = aliceKeyExchangeCrypter.encrypt(secret.getBytes(), aliceKeyParameter);
+
+        byte [] decryptedData = aliceKeyExchangeCrypter.decrypt(encryptedData, aliceKeyParameter);
+
+        assertEquals("they should be the same string", new String(decryptedData), secret );
+    }
+}
