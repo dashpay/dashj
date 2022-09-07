617c2f7a631147117aaf4b3bd7a7a2b498c916f9
HashEngineering <hashengineeringsolutions@gmail.com>
2022-09-07 12:12:16 -0700

    fix: license name and copyright

--- core/src/test/java/org/bitcoinj/crypto/KeyCrypterAESCBCTest.java
+++ core/src/test/java/org/bitcoinj/crypto/KeyCrypterAESCBCTest.java
@@ -1,12 +1,11 @@
 /*
- * Copyright 2013 Jim Burton.
- * Copyright 2014 Andreas Schildbach
+ * Copyright 2022 Dash Core Group
  *
- * Licensed under the MIT license (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://opensource.org/licenses/mit-license.php
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
