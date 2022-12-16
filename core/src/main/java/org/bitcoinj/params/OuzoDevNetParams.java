43db517dec5e79baf83961efca22cd05a86d4a12
HashEngineering <hashengineeringsolutions@gmail.com>
2022-12-16 08:59:16 -0800

    fix: update Jack Daniels and add Ouzo devnets

--- core/src/main/java/org/bitcoinj/params/OuzoDevNetParams.java
+++ core/src/main/java/org/bitcoinj/params/OuzoDevNetParams.java
@@ -18,26 +18,32 @@ package org.bitcoinj.params;
 
 import org.bitcoinj.quorums.LLMQParameters;
 
-public class JackDanielsDevNetParams extends DevNetParams {
-    private static final String DEVNET_NAME = "jack-daniels";
+public class OuzoDevNetParams extends DevNetParams {
+    private static final String DEVNET_NAME = "ouzo";
 
     private static final String[] MASTERNODES = new String[] {
-        "34.220.200.8",
-        "35.90.255.217",
-        "54.218.109.249",
-        "35.91.227.162",
-        "34.222.40.218",
-        "35.88.38.193",
-        "35.91.226.251",
-        "35.160.157.3",
-        "18.237.219.248",
-        "35.91.210.71",
-        "35.89.227.73",
-        "35.90.188.155",
-    };
+                "35.88.153.100",
+                "35.90.86.5",
+                "54.201.101.166",
+                "34.221.211.13",
+                "34.211.239.27",
+                "34.209.234.219",
+                "34.217.126.166",
+                "54.149.125.171",
+                "34.221.134.128",
+                "35.92.44.96",
+                "54.188.181.173",
+                "35.88.196.32",
+                "52.13.97.221",
+                "34.218.76.191",
+                "34.221.191.53",
+                "35.89.136.16",
+                "34.220.42.33",
+                "18.237.57.152"
+};
 
-    public JackDanielsDevNetParams() {
-        super(DEVNET_NAME, "yYBanbwp2Pp2kYWqDkjvckY3MosuZzkKp7", 20001,
+    public OuzoDevNetParams() {
+        super(DEVNET_NAME, "yMu45N9hjWm6YZfonsnasugkiaL5ihrkkc", 20001,
                 MASTERNODES, true, 70220);
         dnsSeeds = MASTERNODES;
         dropPeersAfterBroadcast = false; // this network is too small
@@ -52,11 +58,11 @@ public class JackDanielsDevNetParams extends DevNetParams {
         addLLMQ(LLMQParameters.LLMQType.LLMQ_DEVNET_DIP0024);
     }
 
-    private static JackDanielsDevNetParams instance;
+    private static OuzoDevNetParams instance;
 
-    public static JackDanielsDevNetParams get() {
+    public static OuzoDevNetParams get() {
         if (instance == null) {
-            instance = new JackDanielsDevNetParams();
+            instance = new OuzoDevNetParams();
             add(instance);
         }
         return instance;
