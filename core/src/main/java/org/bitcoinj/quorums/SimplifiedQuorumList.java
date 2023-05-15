728eb7caa468c454625c80955cf2fe85f07e6441
HashEngineering <hashengineeringsolutions@gmail.com>
2023-05-15 09:51:14 -0700

    fix: allow large quorums to be older than the blocks in the blockstore

--- core/src/main/java/org/bitcoinj/quorums/SimplifiedQuorumList.java
+++ core/src/main/java/org/bitcoinj/quorums/SimplifiedQuorumList.java
@@ -178,7 +178,9 @@ public class SimplifiedQuorumList extends Message {
             //if quorum was created before DIP8 activation, then allow it to be added
             if (chainHeight >= params.getDIP0008BlockHeight() || !isLoadingBootstrap) {
                 //for some reason llmqType 2 quorumHashs are from block 72000, which is before DIP8 on testnet.
-                if (!params.getId().equals(NetworkParameters.ID_TESTNET) && !isFirstQuorumCheck && entry.llmqType != 2 && !params.getAssumeValidQuorums().contains(entry.quorumHash)) {
+                if (!params.getId().equals(NetworkParameters.ID_TESTNET) && !isFirstQuorumCheck &&
+                        (entry.llmqType != LLMQParameters.LLMQType.LLMQ_400_60.value && entry.llmqType != LLMQParameters.LLMQType.LLMQ_400_85.value) &&
+                        !params.getAssumeValidQuorums().contains(entry.quorumHash)) {
                     throw new ProtocolException("QuorumHash not found: " + entry.quorumHash);
                 }
             }
