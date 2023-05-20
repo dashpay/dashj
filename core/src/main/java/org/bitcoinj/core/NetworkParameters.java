61e448501e8d7c4e74c1eb99af1d79ce3fc8ca93
HashEngineering <hashengineeringsolutions@gmail.com>
2023-05-19 21:40:08 -0700

    fix: set minimum protocol to 70227 to ban Core 18 nodes

--- core/src/main/java/org/bitcoinj/core/NetworkParameters.java
+++ core/src/main/java/org/bitcoinj/core/NetworkParameters.java
@@ -661,7 +661,7 @@ public abstract class NetworkParameters {
     public abstract int getProtocolVersionNum(final ProtocolVersion version);
 
     public static enum ProtocolVersion {
-        MINIMUM(70220),
+        MINIMUM(70227),
         PONG(60001),
         BLOOM_FILTER(MINIMUM.getBitcoinProtocolVersion()),
         BLOOM_FILTER_BIP111(MINIMUM.getBitcoinProtocolVersion()+1),
