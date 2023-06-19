3d481ef76aff9707d2936bf2a7e4bb1671b8dc2b
HashEngineering <hashengineeringsolutions@gmail.com>
2023-06-19 08:59:18 -0700

    fix: add checkpoint to TestNet3Params for 850100 (blockchain rewind)
    Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

--- core/src/main/java/org/bitcoinj/params/TestNet3Params.java
+++ core/src/main/java/org/bitcoinj/params/TestNet3Params.java
@@ -77,6 +77,7 @@ public class TestNet3Params extends AbstractBitcoinNetParams {
         checkpoints.put(470000, Sha256Hash.wrap("0000009303aeadf8cf3812f5c869691dbd4cb118ad20e9bf553be434bafe6a52"));
         checkpoints.put(794950, Sha256Hash.wrap("000001860e4c7248a9c5cc3bc7106041750560dc5cd9b3a2641b49494bcff5f2"));
         checkpoints.put(808000, Sha256Hash.wrap("00000104cb60a2b5e00a8a4259582756e5bf0dca201c0993c63f0e54971ea91a"));
+        checkpoints.put(850100, Sha256Hash.wrap("000004728b8ff2a16b9d4eebb0fd61eeffadc9c7fe4b0ec0b5a739869401ab5b"));
 
         // updated with Dash Core 0.17.0.3 seed list
         addrSeeds = new int[]{
