4c687b426c663f90cfc2a36878c437c0e27e51c8
HashEngineering <hashengineeringsolutions@gmail.com>
2023-06-20 06:57:13 -0700

    fix: fix setProtocolVersion method in MessageSerializer
    Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

--- core/src/main/java/org/bitcoinj/core/MessageSerializer.java
+++ core/src/main/java/org/bitcoinj/core/MessageSerializer.java
@@ -41,7 +41,7 @@ public abstract class MessageSerializer {
     public void setProtocolVersion(int protocolVersion) {
         this.protocolVersion = Math.min(protocolVersion, NetworkParameters.ProtocolVersion.CURRENT.getBitcoinProtocolVersion());
         // the version we select should >= to MINIMUM
-        this.protocolVersion = Math.min(this.protocolVersion, NetworkParameters.ProtocolVersion.MINIMUM.getBitcoinProtocolVersion());
+        this.protocolVersion = Math.max(this.protocolVersion, NetworkParameters.ProtocolVersion.MINIMUM.getBitcoinProtocolVersion());
     }
 
     protected MessageSerializer() {
