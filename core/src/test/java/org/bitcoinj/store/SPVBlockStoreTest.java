bitcoinj/bitcoinj

WalletAppKit: Clear the blockchain file (as opposed to delete it) in preparation for restore.
This adds a new clear() method to SPVBlockStore.

core/src/test/java/org/bitcoinj/store/SPVBlockStoreTest.java
@@ -19,6 +19,7 @@ package org.bitcoinj.store;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
 
 import java.io.File;
 import java.math.BigInteger;
@@ -144,6 +145,23 @@ public class SPVBlockStoreTest {
         }
         assertTrue("took " + watch + " for " + ITERATIONS + " iterations",
                 watch.elapsed(TimeUnit.MILLISECONDS) < THRESHOLD_MS);
+        store.close();
+    }
+
+    @Test
+    public void clear() throws Exception {
+        SPVBlockStore store = new SPVBlockStore(UNITTEST, blockStoreFile);
+
+        // Build a new block.
+        Address to = LegacyAddress.fromKey(UNITTEST, new ECKey());
+        StoredBlock genesis = store.getChainHead();
+        StoredBlock b1 = genesis.build(genesis.getHeader().createNextBlock(to).cloneAsHeader());
+        store.put(b1);
+        store.setChainHead(b1);
+        assertEquals(b1.getHeader().getHash(), store.getChainHead().getHeader().getHash());
+        store.clear();
+        assertNull(store.get(b1.getHeader().getHash()));
+        assertEquals(UNITTEST.getGenesisBlock().getHash(), store.getChainHead().getHeader().getHash());
         store.close();
     }
 }

dashpay/dashj

(cherry picked from commit 6c944f41422751bf98aed260377d38be3ef9e486)
Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

core/src/test/java/org/bitcoinj/store/SPVBlockStoreTest.java
@@ -19,6 +19,7 @@ package org.bitcoinj.store;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
 
 import java.io.File;
 import java.math.BigInteger;
@@ -153,6 +154,23 @@ public class SPVBlockStoreTest {
         }
         assertTrue("took " + watch + " for " + ITERATIONS + " iterations",
                 watch.elapsed(TimeUnit.MILLISECONDS) < THRESHOLD_MS);
+        store.close();
+    }
+
+    @Test
+    public void clear() throws Exception {
+        SPVBlockStore store = new SPVBlockStore(UNITTEST, blockStoreFile);
+
+        // Build a new block.
+        Address to = Address.fromKey(UNITTEST, new ECKey());
+        StoredBlock genesis = store.getChainHead();
+        StoredBlock b1 = genesis.build(genesis.getHeader().createNextBlock(to).cloneAsHeader());
+        store.put(b1);
+        store.setChainHead(b1);
+        assertEquals(b1.getHeader().getHash(), store.getChainHead().getHeader().getHash());
+        store.clear();
+        assertNull(store.get(b1.getHeader().getHash()));
+        assertEquals(UNITTEST.getGenesisBlock().getHash(), store.getChainHead().getHeader().getHash());
         store.close();
     }
 }