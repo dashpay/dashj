bitcoinj/bitcoinj

In the tests, change all assertTrue(Arrays.equals()) to assertArrayEquals().

core/src/test/java/org/bitcoinj/core/BlockTest.java
@@ -135,7 +135,7 @@ public class BlockTest {
         // proves that transaction serialization works, along with all its subobjects like scripts and in/outpoints.
         //
         // NB: This tests the bitcoin serialization protocol.
-        assertTrue(Arrays.equals(block700000Bytes, block700000.bitcoinSerialize()));
+        assertArrayEquals(block700000Bytes, block700000.bitcoinSerialize());
     }
     
     @Test

dashpay/dashj

(cherry picked from commit e34766449424524b9999a2542a70aafdf8a84ddb)
Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

core/src/test/java/org/bitcoinj/core/BlockTest.java
@@ -141,7 +141,7 @@ public class BlockTest {
         // proves that transaction serialization works, along with all its subobjects like scripts and in/outpoints.
         //
         // NB: This tests the bitcoin serialization protocol.
-        assertTrue(Arrays.equals(blockBytes, block.bitcoinSerialize()));
+        assertArrayEquals(blockBytes, block.bitcoinSerialize());
     }
     
     @Test