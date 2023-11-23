6b91b0566d73516a755ff4d7a3ed3bc404d6e978
HashEngineering <hashengineeringsolutions@gmail.com>
2023-11-22 17:33:02 -0800

    fix: update logs in SimplifiedQuorumList and DownloadProgressTracker for mnlistdiff
    Signed-off-by: HashEngineering <hashengineeringsolutions@gmail.com>

--- core/src/main/java/org/bitcoinj/core/listeners/DownloadProgressTracker.java
+++ core/src/main/java/org/bitcoinj/core/listeners/DownloadProgressTracker.java
@@ -141,7 +141,7 @@ public class DownloadProgressTracker extends AbstractPeerDataEventListener {
 
         if (lastMasternodeListStage.ordinal() > Stage.BeforeStarting.ordinal() &&
             lastMasternodeListStage.ordinal() < Stage.Complete.ordinal()) {
-            log.info(String.format(Locale.US, "Chain download %d%% done. Processing Masternode Lists...", (int) pct));
+            log.info(String.format(Locale.US, "Chain download %d%% done while processing Masternode Lists: %s", (int) pct, lastMasternodeListStage));
         } else if (!requiresHeaders || headersCaughtUp) {
             log.info(String.format(Locale.US, "Chain download %d%% done with %d blocks to go, block date %s", (int) pct, blocksSoFar,
                     Utils.dateTimeFormat(date)));
