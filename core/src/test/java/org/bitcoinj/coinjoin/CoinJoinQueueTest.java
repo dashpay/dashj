5c96255d826b04fbf3b01a458bb9441c0b8c1bb8
HashEngineering <hashengineeringsolutions@gmail.com>
2024-03-19 06:42:34 -0700

    tests: add more coinjoin related tests (#243)
    * tests(coinjoin): add tests for CoinJoinEntry, CoinJoinQueue, CoinJoinClientOptions
    
    * tests(coinjoin): rename dsq tests

--- core/src/test/java/org/bitcoinj/coinjoin/CoinJoinQueueTest.java
+++ core/src/test/java/org/bitcoinj/coinjoin/CoinJoinQueueTest.java
@@ -25,6 +25,7 @@ import org.bitcoinj.crypto.BLSPublicKey;
 import org.bitcoinj.crypto.BLSScheme;
 import org.bitcoinj.crypto.BLSSecretKey;
 import org.bitcoinj.params.UnitTestParams;
+import org.bouncycastle.util.encoders.Hex;
 import org.dashj.bls.BLSJniLibrary;
 import org.dashj.bls.PrivateKey;
 import org.junit.Before;
@@ -68,6 +69,43 @@ public class CoinJoinQueueTest {
         assertArrayEquals(payload, queueFromCtor.bitcoinSerialize());
         BLSPublicKey masternodeOperatorKey = new BLSPublicKey(Utils.HEX.decode("066d57a6451b7800c1c2a6c6e04fe73ec2e1c95e492bacae760ad2f79ca3c30727ec9bf0daea43c08ff1ad6c2cf07612"), true);
         assertTrue(queueFromCtor.checkSignature(masternodeOperatorKey));
+        assertFalse(queueFromCtor.isTried());
+        queueFromCtor.setTried(true);
+        assertTrue(queueFromCtor.isTried());
+    }
+
+    /*
+        Test using a masternode with a basic operator key and a basic signature
+     */
+    @Test
+    public void queueWithBasicMasternodePass() {
+        byte[] payload = Utils.HEX.decode("08000000e6a70ae4131ee990a4c014421685e9a3f4ad1197f8e4f247eb4121905af74a3a17779865000000000160a9852a8fe5d7d4c1be091adbcc226d06b5446d0b3cf75384c945068eab7ede51cfc64666f0d2e6bee07b5b463b8016e209ea16c3a9faa915ddc1037aa59d5c74f403f8b0da263a97a18f81134be41f55b1b59f16f88c8e44c3801bd236f96444");
+        byte[] masternodePublicKeyBytes = Utils.HEX.decode("a1a2ce03d33508fa6d0d0d106b405824a5a583ce109e1e5513c76d3c70aac13b49ed78980ac7fb0836d391d34c453a5d");
+        CoinJoinQueue queueFromHex = new CoinJoinQueue(PARAMS, payload, PARAMS.getProtocolVersionNum(NetworkParameters.ProtocolVersion.BLS_BASIC));
+        BLSPublicKey masternodePublicKey = new BLSPublicKey(masternodePublicKeyBytes, false);
+
+        assertTrue(queueFromHex.checkSignature(masternodePublicKey));
+        assertEquals(8, queueFromHex.getDenomination());
+        assertArrayEquals(Hex.decode("a9852a8fe5d7d4c1be091adbcc226d06b5446d0b3cf75384c945068eab7ede51cfc64666f0d2e6bee07b5b463b8016e209ea16c3a9faa915ddc1037aa59d5c74f403f8b0da263a97a18f81134be41f55b1b59f16f88c8e44c3801bd236f96444"), queueFromHex.getSignature().getBytes());
+        assertFalse(queueFromHex.isTried());
+        assertTrue(queueFromHex.isTimeOutOfBounds());
+    }
+
+    /*
+        Test using a masternode with a legacy operator key, but a basic signature
+     */
+    @Test
+    public void queueWithLegacyMasternodeTest() {
+        byte[] payload = Utils.HEX.decode("04000000d7c0742ff5feecdf9ba16028cdd20ce6d740e8134e726967b472e57c4e80e50a888698650000000001609477d10b679ac351aed7af38747b513847cb60b422aca6982f1ae9c1dc8a07e7a29c9d97b53cfdced846997f045079510924bbe9baf3a074d1ee495c36fbc968d1a632a9189297e5e9fd820343448ed3b559d39b039b13fb6499216fa95aa2bc");
+        byte[] masternodePublicKeyBytes = Utils.HEX.decode("980d9cbfe63468e27b06bb20224f9f5a443a3a8d31fd4e7d52412121c5b7b2f6036089eae3dbbf36a1a7fa2fc1de654c");
+        CoinJoinQueue queueFromHex = new CoinJoinQueue(PARAMS, payload, PARAMS.getProtocolVersionNum(NetworkParameters.ProtocolVersion.BLS_BASIC));
+        BLSPublicKey masternodePublicKey = new BLSPublicKey(masternodePublicKeyBytes, true);
+
+        assertTrue(queueFromHex.checkSignature(masternodePublicKey));
+        assertEquals(4, queueFromHex.getDenomination());
+        assertArrayEquals(Hex.decode("9477d10b679ac351aed7af38747b513847cb60b422aca6982f1ae9c1dc8a07e7a29c9d97b53cfdced846997f045079510924bbe9baf3a074d1ee495c36fbc968d1a632a9189297e5e9fd820343448ed3b559d39b039b13fb6499216fa95aa2bc"), queueFromHex.getSignature().getBytes());
+        assertFalse(queueFromHex.isTried());
+        assertTrue(queueFromHex.isTimeOutOfBounds());
     }
 
     @Test
