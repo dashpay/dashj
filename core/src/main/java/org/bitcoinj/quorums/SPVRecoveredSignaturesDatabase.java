7469b7f28847e5f0251789f85a8f30beae35681a
HashEngineering <hashengineeringsolutions@gmail.com>
2025-07-01 16:59:39 -0700

    fix: clean up SPVRecoveredSignaturesDatabase

--- core/src/main/java/org/bitcoinj/quorums/SPVRecoveredSignaturesDatabase.java
+++ core/src/main/java/org/bitcoinj/quorums/SPVRecoveredSignaturesDatabase.java
@@ -1,10 +1,24 @@
+/*
+ * Copyright 2019 Dash Core Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.bitcoinj.quorums;
 
 import org.bitcoinj.core.*;
 import org.bitcoinj.utils.Pair;
 import org.bitcoinj.utils.Threading;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -14,12 +28,9 @@ import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class SPVRecoveredSignaturesDatabase extends AbstractManager implements RecoveredSignaturesDatabase {
-
-    private static final Logger log = LoggerFactory.getLogger(SPVRecoveredSignaturesDatabase.class);
     private ReentrantLock lock = Threading.lock("SPVRecoveredSignaturesDatabase");
 
-    SigningManager manager;
-    ArrayList<RecoveredSignature> recoveredSignatures;
+    HashMap<Sha256Hash, RecoveredSignature> mapByMessageHash;
     HashMap<Sha256Hash, RecoveredSignature> mapBySignHash;
     HashMap<Pair<Integer, Sha256Hash>, RecoveredSignature> mapById;
     HashMap<Pair<Integer, Sha256Hash>, Sha256Hash> mapVotes;
@@ -31,24 +42,20 @@ public class SPVRecoveredSignaturesDatabase extends AbstractManager implements R
 
     public SPVRecoveredSignaturesDatabase(Context context) {
         super(context);
-        recoveredSignatures = new ArrayList<RecoveredSignature>();
-        hasSigForHashCache = new HashMap<Sha256Hash, Boolean>();
-        hasSigForIdCache = new HashMap<Pair<Integer, Sha256Hash>, Boolean>();
-        hasSigForSessionCache = new HashMap<Sha256Hash, Boolean>();
-        mapBySignHash = new HashMap<Sha256Hash, RecoveredSignature>();
-        mapById = new HashMap<Pair<Integer, Sha256Hash>, RecoveredSignature>();
-        mapVotes = new HashMap<Pair<Integer, Sha256Hash>, Sha256Hash>();
-        mapTimeStamps = new HashMap<Sha256Hash, Long>();
+        hasSigForHashCache = new HashMap<>();
+        hasSigForIdCache = new HashMap<>();
+        hasSigForSessionCache = new HashMap<>();
+        mapByMessageHash = new HashMap<>();
+        mapBySignHash = new HashMap<>();
+        mapById = new HashMap<>();
+        mapVotes = new HashMap<>();
+        mapTimeStamps = new HashMap<>();
     }
 
     public boolean hasRecoveredSig(LLMQParameters.LLMQType llmqType, Sha256Hash id, Sha256Hash msgHash){
         lock.lock();
         try {
-            for(RecoveredSignature recSig : recoveredSignatures) {
-                if(recSig.llmqType == llmqType.getValue() && recSig.id.equals(id) && recSig.msgHash.equals(msgHash))
-                    return true;
-            }
-            return false;
+            return mapByMessageHash.containsKey(msgHash);
         } finally {
             lock.unlock();
         }
@@ -57,8 +64,7 @@ public class SPVRecoveredSignaturesDatabase extends AbstractManager implements R
     public boolean hasRecoveredSigForId(LLMQParameters.LLMQType llmqType, Sha256Hash id) {
         lock.lock();
         try {
-            Boolean result = hasSigForIdCache.get(new Pair(llmqType.getValue(), id));
-            return result != null ? result : false;
+            return hasSigForIdCache.containsKey(new Pair<>(llmqType.getValue(), id));
         } finally {
             lock.unlock();
         }
@@ -67,8 +73,7 @@ public class SPVRecoveredSignaturesDatabase extends AbstractManager implements R
     public boolean hasRecoveredSigForSession(Sha256Hash signHash) {
         lock.lock();
         try {
-            Boolean result = hasSigForSessionCache.get(signHash);
-            return result != null ? result : false;
+            return hasSigForSessionCache.containsKey(signHash);
         } finally {
             lock.unlock();
         }
@@ -92,7 +97,7 @@ public class SPVRecoveredSignaturesDatabase extends AbstractManager implements R
     public RecoveredSignature getRecoveredSigById(LLMQParameters.LLMQType llmqType, Sha256Hash id) {
         lock.lock();
         try {
-            return mapById.get(new Pair(llmqType.value, id));
+            return mapById.get(new Pair<>(llmqType.getValue(), id));
         } finally {
             lock.unlock();
         }
@@ -101,52 +106,50 @@ public class SPVRecoveredSignaturesDatabase extends AbstractManager implements R
     public void writeRecoveredSig(RecoveredSignature recSig) {
         lock.lock();
         try {
+            mapByMessageHash.put(recSig.msgHash, recSig);
             Sha256Hash signHash = LLMQUtils.buildSignHash(recSig.llmqType, recSig.quorumHash, recSig.id, recSig.id);
-            recoveredSignatures.add(recSig);
             mapBySignHash.put(signHash, recSig);
-            Pair<Integer, Sha256Hash> idPair = new Pair(recSig.llmqType, recSig.id);
+            Pair<Integer, Sha256Hash> idPair = new Pair<>(recSig.llmqType, recSig.id);
             mapById.put(idPair, recSig);
             hasSigForIdCache.put(idPair, true);
             hasSigForSessionCache.put(signHash, true);
             hasSigForHashCache.put(recSig.getHash(), true);
             mapTimeStamps.put(signHash, Utils.currentTimeMillis());
-            //save();
         } finally {
             lock.unlock();
         }
     }
 
     public void cleanupOldRecoveredSignatures(long maxAge) {
+        lock.lock();
+        try {
+            ArrayList<Sha256Hash> toDelete = new ArrayList<>();
 
-            lock.lock();
-            try {
-                ArrayList<Sha256Hash> toDelete = new ArrayList<Sha256Hash>();
-
-                for(Map.Entry<Sha256Hash, Long> entry : mapTimeStamps.entrySet()) {
-                    if(entry.getValue() > maxAge)
-                        toDelete.add(entry.getKey());
-                }
-
-                for(Sha256Hash signHash : toDelete) {
-                    RecoveredSignature recSig = mapBySignHash.get(signHash);
-                    hasSigForIdCache.remove(new Pair(recSig.llmqType, recSig.id));
-                    hasSigForSessionCache.remove(signHash);
-                    hasSigForHashCache.remove(recSig.getHash());
-                    mapTimeStamps.remove(signHash);
-                    recoveredSignatures.remove(recSig);
-                    mapBySignHash.remove(signHash);
-                    mapById.remove(new Pair(recSig.llmqType, recSig.id));
-                }
-            } finally {
-                lock.unlock();
+            for(Map.Entry<Sha256Hash, Long> entry : mapTimeStamps.entrySet()) {
+                if(entry.getValue() < maxAge)
+                    toDelete.add(entry.getKey());
             }
+
+            for(Sha256Hash signHash : toDelete) {
+                RecoveredSignature recSig = mapBySignHash.get(signHash);
+                mapByMessageHash.remove(recSig.msgHash);
+                hasSigForIdCache.remove(new Pair<>(recSig.llmqType, recSig.id));
+                hasSigForSessionCache.remove(signHash);
+                hasSigForHashCache.remove(recSig.getHash());
+                mapTimeStamps.remove(signHash);
+                mapBySignHash.remove(signHash);
+                mapById.remove(new Pair<>(recSig.llmqType, recSig.id));
+            }
+        } finally {
+            lock.unlock();
         }
+    }
 
     // votes are removed when the recovered sig is written to the db
     public boolean hasVotedOnId(LLMQParameters.LLMQType llmqType, Sha256Hash id) {
         lock.lock();
         try {
-            return mapVotes.containsKey(new Pair(llmqType.value, id));
+            return mapVotes.containsKey(new Pair<>(llmqType.getValue(), id));
         } finally {
             lock.unlock();
         }
@@ -154,7 +157,7 @@ public class SPVRecoveredSignaturesDatabase extends AbstractManager implements R
     public Sha256Hash getVoteForId(LLMQParameters.LLMQType llmqType, Sha256Hash id) {
         lock.lock();
         try {
-            return mapVotes.get(new Pair(llmqType.value, id));
+            return mapVotes.get(new Pair<>(llmqType.getValue(), id));
         } finally {
             lock.unlock();
         }
@@ -162,7 +165,7 @@ public class SPVRecoveredSignaturesDatabase extends AbstractManager implements R
     public void writeVoteForId(LLMQParameters.LLMQType llmqType, Sha256Hash id, Sha256Hash msgHash) {
         lock.lock();
         try {
-            mapVotes.put(new Pair(llmqType.value, id), msgHash);
+            mapVotes.put(new Pair<>(llmqType.getValue(), id), msgHash);
         } finally {
             lock.unlock();
         }
@@ -170,17 +173,12 @@ public class SPVRecoveredSignaturesDatabase extends AbstractManager implements R
 
     @Override
     protected void parse() throws ProtocolException {
-
+        // there is nothing to load
     }
 
     @Override
     protected void bitcoinSerializeToStream(OutputStream stream) throws IOException {
-        lock.lock();
-        try {
-
-        } finally {
-            lock.unlock();
-        }
+        // there is nothing to save
     }
 
     @Override
@@ -190,12 +188,19 @@ public class SPVRecoveredSignaturesDatabase extends AbstractManager implements R
 
     @Override
     public void checkAndRemove() {
-
+        // there is nothing to check and remove
     }
 
     @Override
     public void clear() {
-
+        hasSigForHashCache.clear();
+        hasSigForIdCache.clear();
+        hasSigForSessionCache.clear();
+        mapBySignHash.clear();
+        mapByMessageHash.clear();
+        mapById.clear();
+        mapVotes.clear();
+        mapTimeStamps.clear();
     }
 
     @Override
@@ -205,6 +210,6 @@ public class SPVRecoveredSignaturesDatabase extends AbstractManager implements R
 
     @Override
     public void close() {
-
+        // there is nothing to close
     }
 }
