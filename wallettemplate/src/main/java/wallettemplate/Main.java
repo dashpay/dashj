c7768b05f3b5ecc832ce4a9311dad6481c62c6f1
HashEngineering <hashengineeringsolutions@gmail.com>
2026-02-22 16:07:41 -0800

    fix: WalletTemplate use ScriptType.PRPKH

--- wallettemplate/src/main/java/wallettemplate/Main.java
+++ wallettemplate/src/main/java/wallettemplate/Main.java
@@ -45,7 +45,7 @@ import static wallettemplate.utils.GuiUtils.*;
 
 public class Main extends Application {
     public static NetworkParameters params = MainNetParams.get();
-    public static final Script.ScriptType PREFERRED_OUTPUT_SCRIPT_TYPE = Script.ScriptType.P2WPKH;
+    public static final Script.ScriptType PREFERRED_OUTPUT_SCRIPT_TYPE = Script.ScriptType.P2PKH;
     public static final String APP_NAME = "WalletTemplate";
     private static final String WALLET_FILE_NAME = APP_NAME.replaceAll("[^a-zA-Z0-9.-]", "_") + "-"
             + params.getPaymentProtocolId();
