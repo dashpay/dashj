bitcoinj/bitcoinj

AppDataDirectory: Move from wallettemplate/.utils to core/.utils
This allows usage in both wallettemplate and tools and replaces
similar code in BlockFileLoader.java in core.
Requires minor back-port of Path.of() functionality from JDK11.

core/src/main/java/org/bitcoinj/utils/BlockFileLoader.java
@@ -71,16 +71,9 @@ public class BlockFileLoader implements Iterable<Block>, Iterator<Block> {
     }
 
     public static File defaultBlocksDir() {
-        final File defaultBlocksDir;
-        if (Utils.isWindows()) {
-            defaultBlocksDir = new File(System.getenv("APPDATA") + "\\.bitcoin\\blocks\\");
-        } else if (Utils.isMac()) {
-            defaultBlocksDir = new File(System.getProperty("user.home") + "/Library/Application Support/Bitcoin/blocks/");
-        } else if (Utils.isLinux()) {
-            defaultBlocksDir = new File(System.getProperty("user.home") + "/.bitcoin/blocks/");
-        } else {
-            throw new RuntimeException("Unsupported system");
-        }
+        File defaultBlocksDir = AppDataDirectory.getPath("Bitcoin").resolve("blocks").toFile();
+        if (!defaultBlocksDir.isDirectory())
+            throw new RuntimeException("Default blocks directory not found");
         return defaultBlocksDir;
     }
 

dashpay/dashj

(cherry picked from commit 3c5744a6cb19d2aa0d0ac3851d551f4174c26798)

core/src/main/java/org/bitcoinj/utils/BlockFileLoader.java
@@ -71,16 +71,9 @@ public class BlockFileLoader implements Iterable<Block>, Iterator<Block> {
     }
 
     public static File defaultBlocksDir() {
-        final File defaultBlocksDir;
-        if (Utils.isWindows()) {
-            defaultBlocksDir = new File(System.getenv("APPDATA") + "\\DashCore\\blocks\\");
-        } else if (Utils.isMac()) {
-            defaultBlocksDir = new File(System.getProperty("user.home") + "/Library/Application Support/DashCore/blocks/");
-        } else if (Utils.isLinux()) {
-            defaultBlocksDir = new File(System.getProperty("user.home") + "/.dashcore/blocks/");
-        } else {
-            throw new RuntimeException("Unsupported system");
-        }
+        File defaultBlocksDir = AppDataDirectory.getPath("DashCore").resolve("blocks").toFile();
+        if (!defaultBlocksDir.isDirectory())
+            throw new RuntimeException("Default blocks directory not found");
         return defaultBlocksDir;
     }
 